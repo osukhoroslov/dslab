249c7d4a7a86f0ca531551e463872e5f9136ccb6
Maksim Gorokhovskii <magor1744@gmail.com>
2021-11-15 14:10:41 +0300

    add multithreading coefficient

--- examples/compute_multicore/src/main.rs
+++ examples/compute_multicore/src/main.rs
@@ -40,22 +40,49 @@ impl Allocation {
     }
 }
 
+// [1 .. max_cores] -> [0, 1]
+#[derive(Debug, Clone)]
+pub enum CoresDependency {
+    Linear,
+    LinearWithFixed {
+        fixed_part: f64,
+    },
+    Custom {
+        func: fn(u64) -> f64,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub enum FailReason {
+    NotEnoughResources {
+        available_cores: u64,
+        available_memory: u64,
+    },
+    Other {
+        reason: String,
+    },
+}
+
 #[derive(Debug, Clone)]
 pub enum Event {
     Start {
     },
     CompRequest {
         computation: Computation,
-        cores: u64,
+        min_cores: u64,
+        max_cores: u64,
+        cores_dependency: CoresDependency,
     },
     CompStarted {
         computation: Computation,
+        cores: u64,
     },
     CompFinished {
         computation: Computation,
     },
     CompFailed {
         computation: Computation,
+        reason: FailReason,
     },
     AllocationRequest {
         allocation: Allocation,
@@ -65,6 +92,7 @@ pub enum Event {
     },
     AllocationFailed {
         allocation: Allocation,
+        reason: FailReason,
     },
     DeallocationRequest {
         allocation: Allocation,
@@ -74,6 +102,7 @@ pub enum Event {
     },
     DeallocationFailed {
         allocation: Allocation,
+        reason: FailReason,
     },
 }
 
@@ -81,14 +110,18 @@ pub enum Event {
 
 pub struct TaskActor {
     task: Computation,
-    cores: u64,
+    min_cores: u64,
+    max_cores: u64,
+    cores_dependency: CoresDependency,
 }
 
 impl TaskActor {
-    pub fn new(computation: Computation, cores: u64) -> Self {
+    pub fn new(computation: Computation, min_cores: u64, max_cores: u64, cores_dependency: CoresDependency) -> Self {
         Self {
             task: computation,
-            cores: cores,
+            min_cores: min_cores,
+            max_cores: max_cores,
+            cores_dependency: cores_dependency,
         }
     }
 }
@@ -99,16 +132,22 @@ impl Actor<Event> for TaskActor {
             Event::Start {} => {
                 println!("{} [{}] received Start from {}", ctx.time(), ctx.id, from);
                 let compute_actor = ActorId::from("compute");
-                ctx.emit(CompRequest { computation: self.task.clone(), cores: self.cores }, compute_actor, 0.);
+                ctx.emit(CompRequest {
+                                       computation: self.task.clone(),
+                                       min_cores: self.min_cores,
+                                       max_cores: self.max_cores,
+                                       cores_dependency: self.cores_dependency.clone(),
+                                     },
+                         compute_actor, 0.);
             }
-            Event::CompStarted { computation } => {
-                println!("{} [{}] received CompStarted from {} for {:?}", ctx.time(), ctx.id, from, computation);
+            Event::CompStarted { computation, cores } => {
+                println!("{} [{}] received CompStarted from {} for {:?} on {} cores", ctx.time(), ctx.id, from, computation, cores);
             },
             Event::CompFinished { computation } => {
                 println!("{} [{}] received CompFinished from {} for {:?}", ctx.time(), ctx.id, from, computation);
             },
-            Event::CompFailed { computation } => {
-                println!("{} [{}] received CompFailed from {} for {:?}", ctx.time(), ctx.id, from, computation);
+            Event::CompFailed { computation, reason } => {
+                println!("{} [{}] received CompFailed from {} for {:?} with reason {:?}", ctx.time(), ctx.id, from, computation, reason);
             },
             _ => ()
         }
@@ -145,11 +184,11 @@ impl Actor<Event> for AllocationActor {
                 ctx.emit(AllocationRequest { allocation: self.allocation.clone() }, compute_actor.clone(), 0.);
                 ctx.emit(DeallocationRequest { allocation: self.allocation.clone() }, compute_actor, self.time);
             }
-            Event::AllocationFailed { allocation } => {
-                println!("{} [{}] received AllocationFailed from {} for {:?}", ctx.time(), ctx.id, from, allocation);
+            Event::AllocationFailed { allocation, reason } => {
+                println!("{} [{}] received AllocationFailed from {} for {:?} with reason {:?}", ctx.time(), ctx.id, from, allocation, reason);
             },
-            Event::DeallocationFailed { allocation } => {
-                println!("{} [{}] received DeallocationFailed from {} for {:?}", ctx.time(), ctx.id, from, allocation);
+            Event::DeallocationFailed { allocation, reason } => {
+                println!("{} [{}] received DeallocationFailed from {} for {:?} with reason {:?}", ctx.time(), ctx.id, from, allocation, reason);
             },
             Event::AllocationSuccess { allocation } => {
                 println!("{} [{}] received AllocationSuccess from {} for {:?}", ctx.time(), ctx.id, from, allocation);
@@ -190,16 +229,30 @@ impl ComputeActor {
 
 impl Actor<Event> for ComputeActor {
     fn on(&mut self, event: Event, from: ActorId, event_id: u64, ctx: &mut ActorContext<Event>) {
+        println!("{} [{}] received {:?} from {}", ctx.time(), ctx.id, event, from);
         match event {
-            Event::CompRequest { ref computation, cores } => {
-                if self.memory < computation.memory || self.cores < cores {
-                    ctx.emit(CompFailed { computation: computation.clone() }, from, 0.);
+            Event::CompRequest { ref computation, min_cores, max_cores, ref cores_dependency } => {
+                if self.memory < computation.memory || self.cores < min_cores {
+                    ctx.emit(CompFailed { computation: computation.clone(), reason: FailReason::NotEnoughResources { available_cores: self.cores, available_memory: self.memory } }, from, 0.);
                 } else {
-                    println!("{} [{}] received {:?} from {}", ctx.time(), ctx.id, event, from);
+                    let cores = self.cores.min(max_cores);
                     self.memory -= computation.memory;
                     self.cores -= cores;
-                    ctx.emit(CompStarted { computation: computation.clone() }, from.clone(), 0.);
-                    let compute_time = computation.flops as f64 / self.speed as f64 / cores as f64;
+                    ctx.emit(CompStarted { computation: computation.clone(), cores }, from.clone(), 0.);
+
+                    let multithreading_coefficient = match cores_dependency {
+                        CoresDependency::Linear => {
+                            1. / cores as f64
+                        },
+                        CoresDependency::LinearWithFixed { fixed_part } => {
+                            fixed_part + (1. - fixed_part) / cores as f64
+                        },
+                        CoresDependency::Custom { func } => {
+                            func(cores)
+                        },
+                    };
+
+                    let compute_time = computation.flops as f64 / self.speed as f64 * multithreading_coefficient;
                     let finish_event_id = ctx.emit(CompFinished { computation: computation.clone() }, ctx.id.clone(), compute_time);
                     self.current_computations.insert(finish_event_id, (cores, from.clone()));
                 }
@@ -212,7 +265,7 @@ impl Actor<Event> for ComputeActor {
             },
             Event::AllocationRequest { allocation } => {
                 if self.memory < allocation.memory || self.cores < allocation.cores {
-                    ctx.emit(AllocationFailed { allocation }, from, 0.);
+                    ctx.emit(AllocationFailed { allocation: allocation, reason: FailReason::NotEnoughResources { available_cores: self.cores, available_memory: self.memory } }, from, 0.);
                 } else {
                     let current_allocation = self.current_allocations.entry(from.clone()).or_insert(Allocation::new(0, 0));
                     current_allocation.cores += allocation.cores;
@@ -231,7 +284,7 @@ impl Actor<Event> for ComputeActor {
                     self.memory += allocation.memory;
                     ctx.emit(DeallocationSuccess { allocation }, from.clone(), 0.);
                 } else {
-                    ctx.emit(DeallocationFailed { allocation }, from.clone(), 0.);
+                    ctx.emit(DeallocationFailed { allocation: allocation, reason: FailReason::NotEnoughResources { available_cores: self.cores, available_memory: self.memory } }, from.clone(), 0.);
                 }
                 if current_allocation.cores == 0 && current_allocation.memory == 0 {
                     self.current_allocations.remove(&from);
@@ -249,23 +302,41 @@ impl Actor<Event> for ComputeActor {
 // MAIN ////////////////////////////////////////////////////////////////////////////////////////////
 
 fn main() {
-    {
-        let mut sim = Simulation::<Event>::new(123);
-        sim.add_actor("task1", Rc::new(RefCell::new(TaskActor::new(Computation::new(100, 512, 1), 2))));
-        sim.add_actor("task2", Rc::new(RefCell::new(TaskActor::new(Computation::new(200, 512, 2), 4))));
-        sim.add_actor("compute", Rc::new(RefCell::new(ComputeActor::new(10, 6, 1024))));
-        sim.add_event(Start {}, "0", "task1", 0.);
-        sim.add_event(Start {}, "0", "task2", 0.);
-        sim.step_until_no_events();
-    }
-    println!("==================================================================================");
-    {
-        let mut sim = Simulation::<Event>::new(123);
-        sim.add_actor("task1", Rc::new(RefCell::new(AllocationActor::new(Allocation::new(2, 512), 3.0))));
-        sim.add_actor("task2", Rc::new(RefCell::new(AllocationActor::new(Allocation::new(5, 512), 4.0))));
-        sim.add_actor("compute", Rc::new(RefCell::new(ComputeActor::new(10, 6, 1024))));
-        sim.add_event(Start {}, "0", "task1", 0.);
-        sim.add_event(Start {}, "0", "task2", 3.1);
-        sim.step_until_no_events();
-    }
+    let mut sim = Simulation::<Event>::new(123);
+    sim.add_actor("task1", Rc::new(RefCell::new(TaskActor::new(
+        Computation::new(100, 512, 1),
+        2,
+        6,
+        CoresDependency::Linear,
+    ))));
+    sim.add_actor("task2", Rc::new(RefCell::new(TaskActor::new(
+        Computation::new(100, 512, 2),
+        4,
+        10,
+        CoresDependency::LinearWithFixed{ fixed_part: 0.4 },
+    ))));
+    sim.add_actor("task3", Rc::new(RefCell::new(TaskActor::new(
+        Computation::new(100, 512, 3),
+        5,
+        7,
+        CoresDependency::Custom{ func: |cores: u64| -> f64 {
+            if cores == 7 {
+                0.5
+            } else {
+                1.0
+            }
+        } },
+    ))));
+    sim.add_actor("task4", Rc::new(RefCell::new(TaskActor::new(
+        Computation::new(100, 512, 4),
+        15,
+        20,
+        CoresDependency::Linear,
+    ))));
+    sim.add_actor("compute", Rc::new(RefCell::new(ComputeActor::new(1, 10, 1024))));
+    sim.add_event(Start {}, "0", "task1", 0.);
+    sim.add_event(Start {}, "0", "task2", 0.);
+    sim.add_event(Start {}, "0", "task3", 1000.);
+    sim.add_event(Start {}, "0", "task4", 2000.);
+    sim.step_until_no_events();
 }
