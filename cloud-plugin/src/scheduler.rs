f62b5a509af90bcc3c0658e046802b0a1ddc4a7d
Vetrov Andrew <47897240+vetand@users.noreply.github.com>
2022-01-08 19:43:00 +0300

    feat cloud-plugin: init (#10)
    * Initial
    
    * Add async
    
    * Add monitoring and review fixes
    
    * Revew 2, part 1
    
    * Review 2, part 2
    
    * Fix bug too early undo reservation
    
    * Add logs
    
    * Review 3, part 1
    
    * Review 3 part 2
    
    * Fix constant bug
    
    * Create lib and total consumed
    
    * Minimal main
    
    * Review 4
    
    * Remove extra

---
+++ cloud-plugin/src/scheduler.rs
@@ -0,0 +1,113 @@
+use core::actor::{Actor, ActorContext, ActorId, Event};
+use core::cast;
+
+use log::info;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+use crate::host::TryAllocateVM;
+use crate::monitoring::HostState;
+use crate::monitoring::Monitoring;
+use crate::virtual_machine::VirtualMachine;
+
+use crate::network::MESSAGE_DELAY;
+
+pub static ALLOCATION_RETRY_PERIOD: f64 = 1.0;
+
+// ACTORS //////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Clone)]
+pub struct Scheduler {
+    pub id: ActorId,
+    monitoring: Rc<RefCell<Monitoring>>,
+    host_states: HashMap<String, HostState>,
+}
+
+impl Scheduler {
+    pub fn new(id: ActorId, monitoring: Rc<RefCell<Monitoring>>) -> Self {
+        Self {
+            id,
+            monitoring,
+            host_states: HashMap::new(),
+        }
+    }
+}
+
+// EVENTS //////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Clone)]
+pub struct FindHostToAllocateVM {
+    pub vm: VirtualMachine,
+}
+
+#[derive(Debug, Clone)]
+pub struct VMAllocationFailed {
+    pub vm: VirtualMachine,
+}
+
+#[derive(Debug, Clone)]
+pub struct VMFinished {
+    pub host_id: String,
+    pub vm: VirtualMachine,
+}
+
+impl Actor for Scheduler {
+    fn on(&mut self, event: Box<dyn Event>, _from: ActorId, ctx: &mut ActorContext) {
+        cast!(match event {
+            FindHostToAllocateVM { vm } => {
+                // pack via First Fit policy
+                let mut found = false;
+                for host in self.monitoring.borrow().get_hosts_list() {
+                    let mut host_state = self
+                        .host_states
+                        .entry(host.to_string())
+                        .or_insert(self.monitoring.borrow().get_host_state(ActorId::from(host)));
+
+                    let cpu_available = host_state.cpu_available;
+                    let ram_available = host_state.ram_available;
+
+                    if cpu_available >= vm.cpu_usage && ram_available >= vm.ram_usage {
+                        info!(
+                            "[time = {}] scheduler #{} decided to pack vm #{} on host #{}",
+                            ctx.time(),
+                            self.id,
+                            vm.id,
+                            host
+                        );
+                        found = true;
+
+                        host_state.cpu_available -= vm.cpu_usage;
+                        host_state.ram_available -= vm.ram_usage;
+
+                        ctx.emit(TryAllocateVM { vm: vm.clone() }, ActorId::from(host), MESSAGE_DELAY);
+                        break;
+                    }
+                }
+                if !found {
+                    info!(
+                        "[time = {}] scheduler #{} failed to pack vm #{}",
+                        ctx.time(),
+                        self.id,
+                        vm.id
+                    );
+
+                    ctx.emit_self(FindHostToAllocateVM { vm: vm.clone() }, ALLOCATION_RETRY_PERIOD);
+                }
+            }
+            VMAllocationFailed { vm } => {
+                ctx.emit_now(FindHostToAllocateVM { vm: vm.clone() }, ctx.id.clone());
+            }
+            VMFinished { host_id, vm } => {
+                self.host_states.get_mut(host_id).map(|state| {
+                    state.cpu_available += vm.cpu_usage;
+                    state.ram_available += vm.ram_usage;
+                });
+            }
+        })
+    }
+
+    fn is_active(&self) -> bool {
+        true
+    }
+}
---
+++ examples/cloud-plugin/src/main.rs
@@ -0,0 +1,33 @@
+use log::info;
+
+use cloud_plugin::simulation::CloudSimulation;
+use core::sim::Simulation;
+
+extern crate env_logger;
+
+fn main() {
+    env_logger::init();
+
+    let sim = Simulation::new(123);
+    let mut cloud_sim = CloudSimulation::new(sim);
+    cloud_sim.init_actors();
+
+    let host_one = cloud_sim.spawn_host("h1", 30, 30);
+    let host_two = cloud_sim.spawn_host("h2", 30, 30);
+    let allocator = cloud_sim.spawn_allocator("a");
+
+    for i in 0..10 {
+        let vm_name = format!("v{}", i);
+        let _vm = cloud_sim.spawn_vm(&vm_name, 10, 10, 2.0, allocator.clone());
+    }
+
+    cloud_sim.steps(170);
+    info!(
+        "Total energy consumed on host one: {} watt",
+        host_one.borrow_mut().get_total_consumed(cloud_sim.current_time())
+    );
+    info!(
+        "Total energy consumed on host two: {} watt",
+        host_two.borrow_mut().get_total_consumed(cloud_sim.current_time())
+    );
+}
