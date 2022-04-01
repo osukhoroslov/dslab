1d8b13e4d623b3f1305e4d3a0ee487c6806e9918
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2022-04-01 19:07:12 +0300

    migrated compute examples

--- examples/compute_singlecore/src/main.rs
+++ examples/compute_singlecore/src/main.rs
@@ -1,21 +1,23 @@
+use env_logger::Builder;
 use std::cell::RefCell;
+use std::io::Write;
 use std::rc::Rc;
 
 use serde::Serialize;
 use sugars::{rc, refcell};
 
 use compute::singlecore::*;
-use core::cast;
 use core::context::SimulationContext;
 use core::event::Event;
 use core::handler::EventHandler;
 use core::simulation::Simulation;
+use core::{cast, log_error, log_info};
 
 #[derive(Serialize)]
 pub struct Start {}
 
 pub struct Task {
-    id: String,
+    id: u32,
     compute: Rc<RefCell<Compute>>,
     flops: u64,
     memory: u64,
@@ -25,7 +27,7 @@ pub struct Task {
 impl Task {
     pub fn new(compute: Rc<RefCell<Compute>>, flops: u64, memory: u64, ctx: SimulationContext) -> Self {
         Self {
-            id: ctx.id().to_string(),
+            id: ctx.id(),
             compute,
             flops,
             memory,
@@ -38,33 +40,30 @@ impl EventHandler for Task {
     fn on(&mut self, event: Event) {
         cast!(match event.data {
             Start {} => {
-                println!("{} [{}] received Start from {}", self.ctx.time(), self.id, event.src);
-                self.compute.borrow_mut().run(self.flops, self.memory, &self.id);
+                log_info!(self.ctx, "received Start from {}", self.ctx.lookup_name(event.src));
+                self.compute.borrow_mut().run(self.flops, self.memory, self.id);
             }
             CompStarted { id } => {
-                println!(
-                    "{} [{}] received CompStarted from {} for {:?}",
-                    self.ctx.time(),
-                    self.id,
-                    event.src,
+                log_info!(
+                    self.ctx,
+                    "received CompStarted from {} for {:?}",
+                    self.ctx.lookup_name(event.src),
                     id
                 );
             }
             CompFinished { id } => {
-                println!(
-                    "{} [{}] received CompFinished from {} for {:?}",
-                    self.ctx.time(),
-                    self.id,
-                    event.src,
+                log_info!(
+                    self.ctx,
+                    "received CompFinished from {} for {:?}",
+                    self.ctx.lookup_name(event.src),
                     id
                 );
             }
             CompFailed { id, reason } => {
-                println!(
-                    "{} [{}] received CompFailed from {} for {:?}, because of {:?}",
-                    self.ctx.time(),
-                    self.id,
-                    event.src,
+                log_error!(
+                    self.ctx,
+                    "received CompFailed from {} for {:?}, because of {:?}",
+                    self.ctx.lookup_name(event.src),
                     id,
                     reason
                 );
@@ -74,19 +73,23 @@ impl EventHandler for Task {
 }
 
 fn main() {
+    Builder::from_default_env()
+        .format(|buf, record| writeln!(buf, "{}", record.args()))
+        .init();
+
     let mut sim = Simulation::new(123);
 
     let compute = rc!(refcell!(Compute::new(10, 1024, sim.create_context("compute"))));
     sim.add_handler("compute", compute.clone());
 
     let task1 = Task::new(compute.clone(), 100, 512, sim.create_context("task1"));
-    sim.add_handler("task1", rc!(refcell!(task1)));
+    let task1_id = sim.add_handler("task1", rc!(refcell!(task1)));
     let task2 = Task::new(compute.clone(), 200, 512, sim.create_context("task2"));
-    sim.add_handler("task2", rc!(refcell!(task2)));
+    let task2_id = sim.add_handler("task2", rc!(refcell!(task2)));
 
     let mut ctx = sim.create_context("root");
-    ctx.emit(Start {}, "task1", 0.);
-    ctx.emit(Start {}, "task2", 5.);
+    ctx.emit(Start {}, task1_id, 0.);
+    ctx.emit(Start {}, task2_id, 5.);
 
     sim.step_until_no_events();
 }
