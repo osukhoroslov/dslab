86adc434aefbe229ccb3f0c3d731c7d9f3313d1c
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2022-04-24 22:57:53 +0300

    improved ping-pong example

--- examples/ping-pong/src/main.rs
+++ examples/ping-pong/src/main.rs
@@ -4,47 +4,49 @@ use std::collections::HashSet;
 use std::io::Write;
 use std::time::Instant;
 
-use clap::{arg, command};
+use clap::Parser;
 use env_logger::Builder;
 use sugars::{rc, refcell};
 
+use network::constant_bandwidth_model::ConstantBandwidthNetwork;
+use network::network::Network;
 use simcore::simulation::Simulation;
 
-use crate::process::{Process, Start};
+use crate::process::{NetworkProcess, Process, Start};
 
-// MAIN ////////////////////////////////////////////////////////////////////////////////////////////
+/// Ping-pong example
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Number of processes (>= 2)
+    #[clap(long, default_value_t = 2)]
+    proc_count: u32,
+
+    /// Number of process peers (>= 1)
+    #[clap(long, default_value_t = 1)]
+    peer_count: usize,
+
+    /// Asymmetric mode
+    #[clap(long)]
+    asymmetric: bool,
+
+    /// Random delay
+    #[clap(long)]
+    rand_delay: bool,
+
+    /// Use network
+    #[clap(long)]
+    use_network: bool,
+
+    /// Number of iterations (>= 1)
+    #[clap(long, default_value_t = 1)]
+    iterations: u32,
+}
 
 fn main() {
-    let matches = command!()
-        .arg(
-            arg!([PROC_COUNT])
-                .help("Number of processes (>= 2)")
-                .validator(|s| s.parse::<u64>())
-                .default_value("2"),
-        )
-        .arg(
-            arg!([PEER_COUNT])
-                .help("Number of process peers (>= 1)")
-                .validator(|s| s.parse::<u64>())
-                .default_value("1"),
-        )
-        .arg(
-            arg!([ASYMMETRIC])
-                .help("Asymmetric mode")
-                .validator(|s| s.parse::<u8>())
-                .default_value("0"),
-        )
-        .arg(
-            arg!([ITERATIONS])
-                .help("Number of iterations (>= 1)")
-                .validator(|s| s.parse::<u64>())
-                .default_value("1"),
-        )
-        .get_matches();
-    let proc_count = matches.value_of_t("PROC_COUNT").unwrap();
-    let peer_count = matches.value_of_t("PEER_COUNT").unwrap();
-    let asymmetric = matches.value_of_t::<u8>("ASYMMETRIC").unwrap() > 0;
-    let iterations = matches.value_of_t("ITERATIONS").unwrap();
+    let args = Args::parse();
+    let proc_count = args.proc_count;
+    let iterations = args.iterations;
 
     Builder::from_default_env()
         .format(|buf, record| writeln!(buf, "{}", record.args()))
@@ -53,28 +55,55 @@ fn main() {
     let mut sim = Simulation::new(123);
     let mut root = sim.create_context("root");
 
-    for proc_id in 1..=proc_count {
-        let proc_name = format!("proc{}", proc_id);
+    let network_opt = if args.use_network {
+        let network_model = rc!(refcell!(ConstantBandwidthNetwork::new(1000., 0.001)));
+        let network = rc!(refcell!(Network::new(network_model, sim.create_context("net"))));
+        sim.add_handler("net", network.clone());
+        network.borrow_mut().add_node("host1", 1000., 0.);
+        network.borrow_mut().add_node("host2", 1000., 0.);
+        Some(network)
+    } else {
+        None
+    };
+    let id_offset = args.use_network as u32;
+
+    for i in 1..=proc_count {
+        let proc_id = i + id_offset;
+        let proc_name = format!("proc{}", i);
         let mut peers = HashSet::new();
-        if peer_count == 1 {
-            let peer_id = (proc_id % proc_count) + 1;
+        if args.peer_count == 1 {
+            let peer_id = (i % proc_count) + 1 + id_offset;
             peers.insert(peer_id);
         } else {
-            while peers.len() < peer_count {
-                let peer_id = root.gen_range(1..=proc_count);
+            while peers.len() < args.peer_count {
+                let peer_id = root.gen_range(1..=proc_count) + id_offset;
                 if peer_id != proc_id {
                     peers.insert(peer_id);
                 }
             }
         }
-        let is_pinger = !asymmetric || proc_id % 2 == 1;
-        let proc = Process::new(
-            Vec::from_iter(peers),
-            is_pinger,
-            iterations,
-            sim.create_context(&proc_name),
-        );
-        sim.add_handler(&proc_name, rc!(refcell!(proc)));
+        let is_pinger = !args.asymmetric || i % 2 == 1;
+        if let Some(ref network) = network_opt {
+            let proc = NetworkProcess::new(
+                Vec::from_iter(peers),
+                is_pinger,
+                iterations,
+                network.clone(),
+                sim.create_context(&proc_name),
+            );
+            sim.add_handler(&proc_name, rc!(refcell!(proc)));
+            let host = format!("host{}", 2 - i % 2);
+            network.borrow_mut().set_location(proc_id, &host);
+        } else {
+            let proc = Process::new(
+                Vec::from_iter(peers),
+                is_pinger,
+                args.rand_delay,
+                iterations,
+                sim.create_context(&proc_name),
+            );
+            sim.add_handler(&proc_name, rc!(refcell!(proc)));
+        }
         root.emit(Start {}, proc_id, 0.);
     }
 
--- examples/ping-pong/src/process.rs
+++ examples/ping-pong/src/process.rs
@@ -1,9 +1,12 @@
 use serde::Serialize;
+use std::cell::RefCell;
+use std::rc::Rc;
 
+use network::network::Network;
 use simcore::cast;
 use simcore::component::Id;
 use simcore::context::SimulationContext;
-use simcore::event::Event;
+use simcore::event::{Event, EventData};
 use simcore::handler::EventHandler;
 
 #[derive(Serialize)]
@@ -19,16 +22,18 @@ pub struct Process {
     peer_count: usize,
     peers: Vec<Id>,
     is_pinger: bool,
+    rand_delay: bool,
     iterations: u32,
     ctx: SimulationContext,
 }
 
 impl Process {
-    pub fn new(peers: Vec<Id>, is_pinger: bool, iterations: u32, ctx: SimulationContext) -> Self {
+    pub fn new(peers: Vec<Id>, is_pinger: bool, rand_delay: bool, iterations: u32, ctx: SimulationContext) -> Self {
         Self {
             peer_count: peers.len(),
             peers,
             is_pinger,
+            rand_delay,
             iterations,
             ctx,
         }
@@ -41,14 +46,12 @@ impl Process {
             } else {
                 self.peers[0]
             };
-            let delay = self.ctx.rand();
-            self.ctx.emit(Ping {}, peer, delay);
+            self.send(Ping {}, peer);
         }
     }
 
     fn on_ping(&mut self, from: Id) {
-        let delay = self.ctx.rand();
-        self.ctx.emit(Pong {}, from, delay);
+        self.send(Pong {}, from);
     }
 
     fn on_pong(&mut self, from: Id) {
@@ -59,13 +62,90 @@ impl Process {
             } else {
                 from
             };
-            let delay = self.ctx.rand();
-            self.ctx.emit(Ping {}, peer, delay);
+            self.send(Ping {}, peer);
         }
+    }
+
+    fn send<T: EventData>(&mut self, event: T, to: Id) {
+        let delay = if self.rand_delay { self.ctx.rand() } else { 0. };
+        self.ctx.emit(event, to, delay);
     }
 }
 
 impl EventHandler for Process {
+    fn on(&mut self, event: Event) {
+        cast!(match event.data {
+            Start {} => {
+                self.on_start();
+            }
+            Ping {} => {
+                self.on_ping(event.src);
+            }
+            Pong {} => {
+                self.on_pong(event.src);
+            }
+        })
+    }
+}
+
+pub struct NetworkProcess {
+    id: Id,
+    peer_count: usize,
+    peers: Vec<Id>,
+    is_pinger: bool,
+    iterations: u32,
+    net: Rc<RefCell<Network>>,
+    ctx: SimulationContext,
+}
+
+impl NetworkProcess {
+    pub fn new(
+        peers: Vec<Id>,
+        is_pinger: bool,
+        iterations: u32,
+        net: Rc<RefCell<Network>>,
+        ctx: SimulationContext,
+    ) -> Self {
+        Self {
+            id: ctx.id(),
+            peer_count: peers.len(),
+            peers,
+            is_pinger,
+            iterations,
+            net,
+            ctx,
+        }
+    }
+
+    fn on_start(&mut self) {
+        if self.is_pinger {
+            let peer = if self.peer_count > 1 {
+                self.peers[self.ctx.gen_range(0..self.peer_count)]
+            } else {
+                self.peers[0]
+            };
+            self.net.borrow_mut().send_event(Ping {}, self.id, peer);
+        }
+    }
+
+    fn on_ping(&mut self, from: Id) {
+        self.net.borrow_mut().send_event(Pong {}, self.id, from);
+    }
+
+    fn on_pong(&mut self, from: Id) {
+        self.iterations -= 1;
+        if self.iterations > 0 {
+            let peer = if self.peer_count > 1 {
+                self.peers[self.ctx.gen_range(0..self.peer_count)]
+            } else {
+                from
+            };
+            self.net.borrow_mut().send_event(Ping {}, self.id, peer);
+        }
+    }
+}
+
+impl EventHandler for NetworkProcess {
     fn on(&mut self, event: Event) {
         cast!(match event.data {
             Start {} => {
