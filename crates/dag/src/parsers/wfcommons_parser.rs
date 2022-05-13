8cba47f36a4ac7d6fd9f0e0f3b98b68ff579b6c5
Maksim Gorokhovskii <magor1744@gmail.com>
2022-05-14 00:43:29 +0300

    Add DAG benchmark

--- crates/dag/src/parsers/wfcommons_parser.rs
+++ crates/dag/src/parsers/wfcommons_parser.rs
@@ -8,6 +8,18 @@ use compute::multicore::CoresDependency;
 use crate::dag::*;
 
 #[derive(Serialize, Deserialize)]
+struct Cpu {
+    speed: Option<u64>, // MHz
+}
+
+#[derive(Serialize, Deserialize)]
+struct Machine {
+    #[serde(rename = "nodeName")]
+    name: String,
+    cpu: Cpu,
+}
+
+#[derive(Serialize, Deserialize)]
 struct File {
     link: String,
     name: String,
@@ -15,15 +27,27 @@ struct File {
 }
 
 #[derive(Serialize, Deserialize)]
-struct Job {
+struct Task {
     name: String,
     runtime: f64,
     files: Vec<File>,
+    machine: Option<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Workflow {
-    jobs: Vec<Job>,
+    jobs: Option<Vec<Task>>,  // v1.2
+    tasks: Option<Vec<Task>>, // v1.3
+    machines: Vec<Machine>,
+}
+
+impl Workflow {
+    fn tasks(&self) -> &Vec<Task> {
+        if self.jobs.is_some() {
+            return self.jobs.as_ref().unwrap();
+        }
+        self.tasks.as_ref().unwrap()
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -32,29 +56,36 @@ struct Json {
 }
 
 impl DAG {
-    pub fn from_wfcommons(file: &str, flops_coefficient: f64) -> Self {
+    pub fn from_wfcommons(file: &str, reference_flops: f64) -> Self {
         let json: Json = from_str(&std::fs::read_to_string(file).expect(&format!("Can't read file {}", file)))
             .expect(&format!("Can't parse WfCommons json from file {}", file));
         let workflow = json.workflow;
+        let machine_speed: HashMap<String, u64> = workflow
+            .machines
+            .iter()
+            .filter(|m| m.cpu.speed.is_some())
+            .map(|machine| (machine.name.clone(), machine.cpu.speed.unwrap() * 1000000))
+            .collect();
         let mut dag = DAG::new();
         let mut data_items: HashMap<String, usize> = HashMap::new();
-        for job in workflow.jobs.iter() {
-            let task_id = dag.add_task(
-                &job.name,
-                (job.runtime * flops_coefficient) as u64,
-                0,
-                1,
-                1,
-                CoresDependency::Linear,
-            );
-            for file in job.files.iter() {
+        for task in workflow.tasks().iter() {
+            let mut task_size = task.runtime;
+
+            if let Some(machine_speed) = task.machine.as_ref().map(|m| machine_speed.get(m)).flatten() {
+                task_size *= *machine_speed as f64;
+            } else {
+                task_size *= reference_flops;
+            }
+
+            let task_id = dag.add_task(&task.name, task_size as u64, 0, 1, 1, CoresDependency::Linear);
+            for file in task.files.iter() {
                 if file.link == "output" {
                     data_items.insert(file.name.clone(), dag.add_task_output(task_id, &file.name, file.size));
                 }
             }
         }
-        for (task_id, job) in workflow.jobs.iter().enumerate() {
-            for file in job.files.iter() {
+        for (task_id, task) in workflow.tasks().iter().enumerate() {
+            for file in task.files.iter() {
                 if file.link == "input" {
                     if let Some(data_item_id) = data_items.get(&file.name) {
                         dag.add_data_dependency(*data_item_id, task_id);
