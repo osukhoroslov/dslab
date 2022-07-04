7af02d1f3647ef0bc74244598e1e242de6940f23
Yury Semenov <yusemru@gmail.com>
2022-07-04 13:57:23 +0300

    [serverless] OpenDC experiment (#85)
    * added benchmark
    
    * fixed rebase bugs
    
    * added opendc experiment
    
    * fixed review comments
    
    * renamed fields
    
    * added event count

---
+++ examples-other/opendc/faas/ServerlessExperiment.kt
@@ -0,0 +1,110 @@
+// Slightly modified https://github.com/atlarge-research/opendc/blob/6d2b140311057e54622fdcd6cf7f8850c370414c/opendc-experiments/opendc-experiments-serverless20/src/main/kotlin/org/opendc/experiments/serverless/ServerlessExperiment.kt
+package org.opendc.experiments.serverless
+
+import com.typesafe.config.ConfigFactory
+import io.opentelemetry.api.metrics.MeterProvider
+import io.opentelemetry.sdk.metrics.SdkMeterProvider
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import mu.KotlinLogging
+import org.opendc.experiments.serverless.trace.FunctionTraceWorkload
+import org.opendc.experiments.serverless.trace.ServerlessTraceReader
+import org.opendc.harness.dsl.Experiment
+import org.opendc.harness.dsl.anyOf
+import org.opendc.serverless.service.ServerlessService
+import org.opendc.serverless.service.autoscaler.FunctionTerminationPolicyFixed
+import org.opendc.serverless.service.router.RandomRoutingPolicy
+import org.opendc.serverless.simulator.SimFunctionDeployer
+import org.opendc.serverless.simulator.delay.ColdStartModel
+import org.opendc.serverless.simulator.delay.StochasticDelayInjector
+import org.opendc.simulator.compute.SimMachineModel
+import org.opendc.simulator.compute.model.MemoryUnit
+import org.opendc.simulator.compute.model.ProcessingNode
+import org.opendc.simulator.compute.model.ProcessingUnit
+import org.opendc.simulator.core.runBlockingSimulation
+import org.opendc.telemetry.sdk.toOtelClock
+import java.io.File
+import java.util.*
+import kotlin.math.max
+
+/**
+ * A reproduction of the experiments of Soufiane Jounaid's BSc Computer Science thesis:
+ * OpenDC Serverless: Design, Implementation and Evaluation of a FaaS Platform Simulator.
+ */
+public class ServerlessExperiment : Experiment("Serverless") {
+    /**
+     * The logger for this portfolio instance.
+     */
+    private val logger = KotlinLogging.logger {}
+
+    /**
+     * The configuration to use.
+     */
+    private val config = ConfigFactory.load().getConfig("opendc.experiments.serverless20")
+
+    /**
+     * The routing policy to test.
+     */
+    private val routingPolicy by anyOf(RandomRoutingPolicy())
+
+    /**
+     * The cold start models to test.
+     */
+    private val coldStartModel by anyOf(ColdStartModel.AZURE)
+
+    override fun doRun(repeat: Int): Unit = runBlockingSimulation {
+        val meterProvider: MeterProvider = SdkMeterProvider
+            .builder()
+            .setClock(clock.toOtelClock())
+            .build()
+
+        val trace = ServerlessTraceReader().parse(File(config.getString("trace-path")))
+        val traceById = trace.associateBy { it.id }
+        val delayInjector = StochasticDelayInjector(coldStartModel, Random())
+        val deployer = SimFunctionDeployer(clock, this, createMachineModel(), delayInjector) { FunctionTraceWorkload(traceById.getValue(it.name)) }
+        val service =
+            ServerlessService(coroutineContext, clock, meterProvider.get("opendc-serverless"), deployer, routingPolicy, FunctionTerminationPolicyFixed(coroutineContext, clock, timeout = 10 * 60 * 1000))
+        val client = service.newClient()
+
+        coroutineScope {
+            for (entry in trace) {
+                launch {
+                    val function = client.newFunction(entry.id, entry.maxMemory.toLong())
+                    var offset = Long.MIN_VALUE
+
+                    for (sample in entry.samples) {
+                        if (sample.invocations == 0) {
+                            continue
+                        }
+
+                        if (offset < 0) {
+                            offset = sample.timestamp - clock.millis()
+                        }
+
+                        delay(max(0, (sample.timestamp - offset) - clock.millis()))
+
+                        repeat(sample.invocations) {
+                            function.invoke()
+                        }
+                    }
+                }
+            }
+        }
+
+        client.close()
+        service.close()
+    }
+
+    /**
+     * Construct the machine model to test with.
+     */
+    private fun createMachineModel(): SimMachineModel {
+        val cpuNode = ProcessingNode("Intel", "Xeon", "amd64", 2)
+
+        return SimMachineModel(
+            cpus = List(cpuNode.coreCount) { ProcessingUnit(cpuNode, it, 1000.0) },
+            memory = List(4) { MemoryUnit("Crucial", "MTA18ASF4G72AZ-3G2B1", 3200.0, 32_000) }
+        )
+    }
+}
---
+++ examples/faas-opendc-benchmark/src/main.rs
@@ -0,0 +1,52 @@
+use std::cell::RefCell;
+use std::path::Path;
+use std::rc::Rc;
+use std::time::Instant;
+
+use dslab_core::simulation::Simulation;
+use dslab_faas::coldstart::{ColdStartPolicy, FixedTimeColdStartPolicy};
+use dslab_faas::function::{Application, Function};
+use dslab_faas::resource::{ResourceConsumer, ResourceProvider};
+use dslab_faas::simulation::ServerlessSimulation;
+use dslab_faas_extra::opendc_trace::process_opendc_trace;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let trace = process_opendc_trace(Path::new(&args[1]));
+
+    let policy: Option<Rc<RefCell<dyn ColdStartPolicy>>> =
+        Some(Rc::new(RefCell::new(FixedTimeColdStartPolicy::new(120.0 * 60.0, 0.0))));
+    let mut sim = ServerlessSimulation::new(Simulation::new(1), None, policy, None);
+    for _ in 0..18 {
+        let mem = sim.create_resource("mem", 4096);
+        sim.add_host(None, ResourceProvider::new(vec![mem]));
+    }
+    for app in trace.iter() {
+        let mut max_mem = 0;
+        for sample in app.iter() {
+            max_mem = usize::max(max_mem, sample.mem_provisioned);
+        }
+        let mem = sim.create_resource_requirement("mem", max_mem as u64);
+        let app_id = sim.add_app(Application::new(1, 0.5, ResourceConsumer::new(vec![mem])));
+        let fn_id = sim.add_function(Function::new(app_id));
+        for sample in app.iter() {
+            if sample.invocations == 0 {
+                continue;
+            }
+            for _ in 0..sample.invocations {
+                sim.send_invocation_request(fn_id, (sample.exec as f64) / 1000.0, (sample.time as f64) / 1000.0);
+            }
+        }
+    }
+    let t = Instant::now();
+    sim.step_until_no_events();
+    let elapsed = t.elapsed().as_secs_f64();
+    let stats = sim.get_stats();
+    println!(
+        "processed {} invocations and {} events in {:.2} seconds ({:.2} events per sec)",
+        stats.invocations,
+        sim.event_count(),
+        elapsed,
+        (sim.event_count() as f64) / elapsed
+    );
+}
