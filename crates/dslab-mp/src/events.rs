26bb77d31bf3a055d2e528a81e358f7fb4f0c2eb
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2022-11-01 21:13:35 +0300

    removed unused events

--- crates/dslab-mp/src/events.rs
+++ crates/dslab-mp/src/events.rs
@@ -2,13 +2,6 @@ use serde::Serialize;
 
 use crate::message::Message;
 
-#[derive(Serialize)]
-pub struct MessageSent {
-    pub msg: Message,
-    pub src: String,
-    pub dest: String,
-}
-
 #[derive(Serialize, Clone)]
 pub struct MessageReceived {
     pub msg: Message,
@@ -20,13 +13,6 @@ pub struct MessageReceived {
 pub struct LocalMessageReceived {
     pub msg: Message,
     pub dest: String,
-}
-
-#[derive(Serialize)]
-pub struct TimerSet {
-    pub timer_name: String,
-    pub proc_name: String,
-    pub delay: f64,
 }
 
 #[derive(Serialize)]
