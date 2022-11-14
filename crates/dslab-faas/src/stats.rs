9315a4c72fb41efad0183957b641f69f0df8de26
Yury Semenov <yusemru@gmail.com>
2022-11-14 18:03:01 +0300

    added more stats

--- crates/dslab-faas/src/stats.rs
+++ crates/dslab-faas/src/stats.rs
@@ -1,41 +1,144 @@
 use std::collections::HashMap;
-use std::ops::AddAssign;
 
-use num::{ToPrimitive, Zero};
+use order_stat::kth_by;
 
 use crate::invocation::Invocation;
 use crate::resource::ResourceConsumer;
 
-#[derive(Clone, Default)]
-pub struct SampleMetric<T> {
-    data: Vec<T>,
+#[derive(Copy, Clone)]
+pub enum QuantileEstimate {
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    R8,
+    R9,
 }
 
-impl<T> SampleMetric<T> {
-    pub fn add(&mut self, x: T) {
-        self.data.push(x);
+impl QuantileEstimate {
+    pub fn get_h(&self, n: f64, q: f64) -> f64 {
+        debug_assert!(0. <= q && q <= 1.);
+        match *self {
+            QuantileEstimate::R1 => {
+                n * q
+            },
+            QuantileEstimate::R2 => {
+                n * q + 0.5
+            },
+            QuantileEstimate::R3 => {
+                n * q - 0.5
+            },
+            QuantileEstimate::R4 => {
+                n * q
+            },
+            QuantileEstimate::R5 => {
+                n * q + 0.5
+            },
+            QuantileEstimate::R6 => {
+                (n + 1.) * q
+            },
+            QuantileEstimate::R7 => {
+                (n - 1.) * q + 1.
+            },
+            QuantileEstimate::R8 => {
+                (n + 1.0/3.0) * q + 1.0/3.0
+            },
+            QuantileEstimate::R9 => {
+                (n + 0.25) * q + 0.375
+            },
+        }
     }
 }
 
-impl<T> SampleMetric<T>
-where
-    T: AddAssign + Copy + Zero,
-{
-    pub fn sum(&self) -> T {
-        let mut s = T::zero();
+#[derive(Clone, Default)]
+pub struct SampleMetric {
+    data: Vec<f64>,
+}
+
+impl SampleMetric {
+    pub fn add(&mut self, x: f64) {
+        self.data.push(x);
+    }
+
+    pub fn sum(&self) -> f64 {
+        let mut s = 0.0;
         for x in self.data.iter().copied() {
             s += x;
         }
         s
     }
-}
 
-impl<T> SampleMetric<T>
-where
-    T: AddAssign + Copy + Zero + ToPrimitive,
-{
     pub fn mean(&self) -> f64 {
-        self.sum().to_f64().unwrap() / (self.data.len() as f64)
+        self.sum() / (self.data.len() as f64)
+    }
+
+    pub fn min(&self) -> Option<f64> {
+        self.data.iter().copied().reduce(f64::min)
+    }
+
+    pub fn max(&self) -> Option<f64> {
+        self.data.iter().copied().reduce(f64::max)
+    }
+
+    pub fn ordered_statistic(&self, idx: usize) -> f64 {
+        debug_assert!(idx < self.data.len());
+        let mut tmp = self.data.clone();
+        *kth_by(&mut tmp, idx, |x, y| x.total_cmp(y))
+    }
+
+    pub fn quantile(&self, q: f64, estimate: QuantileEstimate) -> f64 {
+        debug_assert!(0. <= q && q <= 1.);
+        let h = estimate.get_h(self.data.len() as f64, q);
+        match estimate {
+            QuantileEstimate::R1 => {
+                let k = (h.ceil() + 1e-9) as usize;
+                self.ordered_statistic(k)
+            },
+            QuantileEstimate::R2 => {
+                let k1 = ((h - 0.5).ceil() + 1e-9) as usize;
+                let k2 = ((h + 0.5).floor() + 1e-9) as usize;
+                (self.ordered_statistic(k1) + self.ordered_statistic(k2)) / 2.0
+            },
+            QuantileEstimate::R3 => {
+                let k = (h.round() + 1e-9) as usize;
+                self.ordered_statistic(k)
+            },
+            _ => {
+                let fl = h.floor();
+                let k1 = (fl + 1e-9) as usize;
+                let k2 = (h.ceil() + 1e-9) as usize;
+                let s1 = self.ordered_statistic(k1);
+                s1 + (h - fl) * (self.ordered_statistic(k2) - s1)
+            }
+        }
+    }
+
+    pub fn variance(&self, biased: bool) -> f64 {
+        let mean = self.mean();
+        let mut var = 0;
+        for x in self.data.iter().copied() {
+            var += (x - mean) * (x - mean);
+        }
+        if biased {
+            var / (self.data.len() as f64)
+        } else {
+            if self.data.len() == 1 {
+                0.0
+            } else {
+                var / ((self.data.len() as f64) - 1.0)
+            }
+        }
+    }
+
+    pub fn biased_variance(&self) -> f64 {
+        self.variance(true)
+    }
+
+    pub fn unbiased_variance(&self) -> f64 {
+        self.variance(false)
     }
 }
 
@@ -43,12 +146,12 @@ where
 pub struct Stats {
     pub invocations: u64,
     pub cold_starts: u64,
-    pub cold_start_latency: SampleMetric<f64>,
-    pub wasted_resource_time: HashMap<usize, SampleMetric<f64>>,
-    pub abs_exec_slowdown: SampleMetric<f64>,
-    pub rel_exec_slowdown: SampleMetric<f64>,
-    pub abs_total_slowdown: SampleMetric<f64>,
-    pub rel_total_slowdown: SampleMetric<f64>,
+    pub cold_start_latency: SampleMetric,
+    pub wasted_resource_time: HashMap<usize, SampleMetric>,
+    pub abs_exec_slowdown: SampleMetric,
+    pub rel_exec_slowdown: SampleMetric,
+    pub abs_total_slowdown: SampleMetric,
+    pub rel_total_slowdown: SampleMetric,
 }
 
 impl Stats {
