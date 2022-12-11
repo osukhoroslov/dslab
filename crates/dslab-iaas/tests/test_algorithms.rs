8787e7f70d5d2f3d501375236e81038cbdfd6189
Ветров Андрей Александрович <aavetrov_2@edu.hse.ru>
2022-12-11 19:35:03 +0300

    Make unique

--- crates/dslab-iaas/tests/test_algorithms.rs
+++ crates/dslab-iaas/tests/test_algorithms.rs
@@ -26,13 +26,13 @@ fn check_placements(algorithm: Box<dyn VMPlacementAlgorithm>, expected_hosts: Ve
     let mut cloud_sim = CloudSimulation::new(sim, sim_config);
 
     let mut host_ids = Vec::<u32>::new();
-    host_ids.push(cloud_sim.add_host("h1", 5, 5));
-    host_ids.push(cloud_sim.add_host("h2", 5, 5));
-    host_ids.push(cloud_sim.add_host("h3", 8, 4));
-    host_ids.push(cloud_sim.add_host("h4", 8, 4));
+    host_ids.push(cloud_sim.add_host("h1", 10, 5));
+    host_ids.push(cloud_sim.add_host("h2", 10, 5));
+    host_ids.push(cloud_sim.add_host("h3", 13, 4));
+    host_ids.push(cloud_sim.add_host("h4", 13, 4));
 
     cloud_sim.spawn_vm_on_host(
-        1,
+        6,
         2,
         10.0,
         Box::new(ConstantLoadModel::new(1.0)),
@@ -41,7 +41,7 @@ fn check_placements(algorithm: Box<dyn VMPlacementAlgorithm>, expected_hosts: Ve
         host_ids[0],
     );
     cloud_sim.spawn_vm_on_host(
-        1,
+        6,
         2,
         10.0,
         Box::new(ConstantLoadModel::new(1.0)),
@@ -50,7 +50,7 @@ fn check_placements(algorithm: Box<dyn VMPlacementAlgorithm>, expected_hosts: Ve
         host_ids[1],
     );
     cloud_sim.spawn_vm_on_host(
-        2,
+        7,
         1,
         10.0,
         Box::new(ConstantLoadModel::new(1.0)),
@@ -59,7 +59,7 @@ fn check_placements(algorithm: Box<dyn VMPlacementAlgorithm>, expected_hosts: Ve
         host_ids[2],
     );
     cloud_sim.spawn_vm_on_host(
-        4,
+        9,
         1,
         10.0,
         Box::new(ConstantLoadModel::new(1.0)),
@@ -131,13 +131,13 @@ fn test_worst_fit() {
 #[test]
 // Tests Dot Product algorithm.
 fn test_dot_product() {
-    check_placements(Box::new(DotProduct::new()), vec!["h3", "h1", "h2"]);
+    check_placements(Box::new(DotProduct::new()), vec!["h3", "h4", "h1"]);
 }
 
 #[test]
 // Tests Weighted Dot Product algorithm.
 fn test_weighted_dot_product() {
-    check_placements(Box::new(WeightedDotProduct::new()), vec!["h3", "h1", "h2"]);
+    check_placements(Box::new(WeightedDotProduct::new()), vec!["h3", "h1", "h4"]);
 }
 
 #[test]
@@ -152,12 +152,12 @@ fn test_l2_norm_diff() {
 // The third host is bigger than the first and the second ones thus the cosine is relatively smaller than
 // the other choices while dot product and perp distance are much bigger due to bigger linear sizes.
 fn test_cosine_similarity() {
-    check_placements(Box::new(CosineSimilarity::new()), vec!["h4", "h3", "h3"]);
+    check_placements(Box::new(CosineSimilarity::new()), vec!["h1", "h3", "h3"]);
 }
 
 #[test]
 // Tests Delta Perp-Distance algorithm.
 // Algorithm skips the third host as long as the balance of resources is already achieved there.
 fn test_delta_perp_distance() {
-    check_placements(Box::new(DeltaPerpDistance::new()), vec!["h4", "h1", "h2"]);
+    check_placements(Box::new(DeltaPerpDistance::new()), vec!["h4", "h3", "h3"]);
 }
