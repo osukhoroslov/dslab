9c3a71c4839f100ff2d448fae9183af50a2202e1
Konstantin Amelichev <kostya.amelichev@gmail.com>
2023-01-16 23:28:24 +0400

    python context docstring

--- crates/dslab-mp-python/python/dslabmp.py
+++ crates/dslab-mp-python/python/dslabmp.py
@@ -35,14 +35,23 @@ class Context(object):
         self._timer_actions: List[Tuple[str, float, bool]] = list()
 
     def send(self, msg: Message, to: str):
+        """
+        Sends message to specified process
+        """
         if not isinstance(to, str):
             raise TypeError('to argument has to be string, not {}'.format(type(to)))
         self._sent_messages.append((msg.type, json.dumps(msg._data), to))
 
     def send_local(self, msg: Message):
+        """
+        Sends a local message
+        """
         self._sent_local_messages.append((msg.type, json.dumps(msg._data)))
 
     def set_timer(self, timer_name: str, delay: float):
+        """
+        Sets timer that will trigger on_timer callback after specified delay. Overrides previous timer with the same name.
+        """
         if not isinstance(timer_name, str):
             raise TypeError('timer_name argument has to be str, not {}'.format(type(timer_name)))
         if not isinstance(delay, (int, float)):
@@ -52,6 +61,9 @@ class Context(object):
         self._timer_actions.append((timer_name, delay, False))
 
     def set_timer_once(self, timer_name: str, delay: float):
+        """
+        Sets timer that will trigger on_timer callback after specified delay. Is ignored if there is an active timer with the same name.
+        """
         if not isinstance(timer_name, str):
             raise TypeError('timer_name argument has to be str, not {}'.format(type(timer_name)))
         if not isinstance(delay, (int, float)):
@@ -61,11 +73,17 @@ class Context(object):
         self._timer_actions.append((timer_name, delay, True))
 
     def cancel_timer(self, timer_name: str):
+        """
+        Cancels timer with specified name.
+        """
         if not isinstance(timer_name, str):
             raise TypeError('timer_name argument has to be str, not {}'.format(type(timer_name)))
         self._timer_actions.append((timer_name, -1, False))
 
     def time(self) -> float:
+        """
+        Returns current system time.
+        """ 
         return self._time
 
 
