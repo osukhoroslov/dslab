217ad50567b88e29e993ab24ed4a928d6b431244
AnthonyUdovichenko <antonyud2000@yandex.ru>
2023-01-29 18:18:41 +0300

    Reduce visibility of SimulationState

--- crates/dslab-mp/src/node.rs
+++ crates/dslab-mp/src/node.rs
@@ -170,7 +170,7 @@ impl Node {
             time,
             ProcessEvent::LocalMessageReceived { msg: msg.clone() },
         ));
-        let mut proc_ctx = Context::new(proc.clone(), self.ctx.clone(), self.clock_skew);
+        let mut proc_ctx = Context::new(proc.clone(), Some(self.ctx.clone()), self.clock_skew);
         proc_entry.proc_impl.on_local_message(msg, &mut proc_ctx);
         self.handle_process_actions(proc, time, proc_ctx.actions());
     }
@@ -188,7 +188,7 @@ impl Node {
             },
         ));
         proc_entry.received_message_count += 1;
-        let mut proc_ctx = Context::new(proc.clone(), self.ctx.clone(), self.clock_skew);
+        let mut proc_ctx = Context::new(proc.clone(), Some(self.ctx.clone()), self.clock_skew);
         proc_entry.proc_impl.on_message(msg, from, &mut proc_ctx);
         self.handle_process_actions(proc, time, proc_ctx.actions());
     }
@@ -198,7 +198,7 @@ impl Node {
         t!(format!("{:>9.3} {:>10} !-- {:<10}", time, proc, timer).yellow());
         let proc_entry = self.processes.get_mut(&proc).unwrap();
         proc_entry.pending_timers.remove(&timer);
-        let mut proc_ctx = Context::new(proc.clone(), self.ctx.clone(), self.clock_skew);
+        let mut proc_ctx = Context::new(proc.clone(), Some(self.ctx.clone()), self.clock_skew);
         proc_entry.proc_impl.on_timer(timer, &mut proc_ctx);
         self.handle_process_actions(proc, time, proc_ctx.actions());
     }
