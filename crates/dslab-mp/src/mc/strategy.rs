5a807100750fd224a78d09972a4ce8d2af20ac2c
AnthonyUdovichenko <antonyud2000@yandex.ru>
2023-02-06 00:43:40 +0300

    Add debug logs

--- crates/dslab-mp/src/mc/strategy.rs
+++ crates/dslab-mp/src/mc/strategy.rs
@@ -1,5 +1,21 @@
+use colored::*;
+
+use crate::mc::events::McEvent;
+use crate::mc::events::McEvent::{MessageReceived, TimerFired};
 use crate::mc::system::McSystem;
+use crate::util::t;
 
 pub trait Strategy {
     fn run(&mut self, system: &mut McSystem) -> bool;
+
+    fn debug_log(event: &McEvent, depth: u64) where Self: Sized {
+        match event {
+            MessageReceived { msg, src, dest } => {
+                t!("{:>10} | {:>10} <-- {:<10} {:?}", depth, dest, src, msg);
+            }
+            TimerFired { proc, timer } => {
+                t!(format!("{:>10} | {:>10} !-- {:<10}", depth, proc, timer).yellow());
+            }
+        }
+    }
 }
