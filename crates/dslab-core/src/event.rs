9bdd14c03e64cd53fc1a619009c6dfc5d0637a36
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2023-02-10 21:38:39 +0300

    [core] Use total_cmp for time comparison

--- crates/dslab-core/src/event.rs
+++ crates/dslab-core/src/event.rs
@@ -45,11 +45,7 @@ impl PartialEq for Event {
 
 impl Ord for Event {
     fn cmp(&self, other: &Self) -> Ordering {
-        other
-            .time
-            .to_bits()
-            .cmp(&self.time.to_bits())
-            .then_with(|| other.id.cmp(&self.id))
+        other.time.total_cmp(&self.time).then_with(|| other.id.cmp(&self.id))
     }
 }
 
