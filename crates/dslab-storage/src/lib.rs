556f338e00044853ac30c66adf9036ae7cd5140f
Tagir Kuskarov <kuskarovtf@mail.ru>
2023-02-20 16:20:00 +0000

    Renamings

--- crates/dslab-storage/src/lib.rs
+++ crates/dslab-storage/src/lib.rs
@@ -1,11 +1,11 @@
 #![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
 
-pub mod bandwidth;
 pub mod events;
 pub mod fs;
 pub mod shared_disk;
 pub mod storage;
+pub mod throughput_factor;
 
 #[cfg(test)]
 mod tests;
