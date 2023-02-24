a1255e43bec74f5b83c58ca16d1fe18d4408ac3f
AnthonyUdovichenko <antonyud2000@yandex.ru>
2023-02-24 23:05:29 +0300

    Add LogContext

--- crates/dslab-mp/src/mc/strategy.rs
+++ crates/dslab-mp/src/mc/strategy.rs
@@ -3,6 +3,7 @@ use colored::*;
 use crate::mc::events::McEvent;
 use crate::mc::events::McEvent::{MessageReceived, TimerFired};
 use crate::mc::system::McSystem;
+use crate::message::Message;
 use crate::util::t;
 use std::collections::HashMap;
 
@@ -10,6 +11,13 @@ use std::collections::HashMap;
 pub enum LogMode {
     Default,
     Debug,
+}
+
+pub enum LogContext {
+    Default,
+    Dropped,
+    Duplicated,
+    Corrupted,
 }
 
 #[derive(Debug, Default, Clone)]
@@ -20,11 +28,13 @@ pub struct McSummary {
 pub trait Strategy {
     fn run(&mut self, system: &mut McSystem) -> Result<McSummary, String>;
 
-    fn debug_log(&self, event: &McEvent, depth: u64) {
+    fn process_event(&mut self, system: &mut McSystem, event_num: usize) -> Result<(), String>;
+
+    fn debug_log(&self, event: &McEvent, depth: u64, log_context: LogContext) {
         if self.log_mode() == &LogMode::Debug {
             match event {
                 MessageReceived { msg, src, dest, .. } => {
-                    t!("{:>10} | {:>10} <-- {:<10} {:?}", depth, dest, src, msg);
+                    self.log_message(depth, msg, src, dest, log_context);
                 }
                 TimerFired { proc, timer } => {
                     t!(format!("{:>10} | {:>10} !-- {:<10}", depth, proc, timer).yellow());
@@ -33,7 +43,30 @@ pub trait Strategy {
         }
     }
 
-    fn process_event(&mut self, system: &mut McSystem, event_num: usize) -> Result<(), String>;
+    fn log_message(&self, depth: u64, msg: &Message, src: &String, dest: &String, log_context: LogContext) {
+        match log_context {
+            LogContext::Default => {
+                t!("{:>10} | {:>10} <-- {:<10} {:?}", depth, dest, src, msg);
+            }
+            LogContext::Dropped => {
+                t!(format!(
+                    "{:>9} {:>10} --x {:<10} {:?} <-- message dropped",
+                    "!!!", src, dest, msg
+                )
+                .red());
+            }
+            LogContext::Duplicated => {
+                t!(format!(
+                    "{:>9} {:>10} -=≡ {:<10} {:?} <-- message duplicated",
+                    "~~~", src, dest, msg
+                )
+                .blue());
+            }
+            LogContext::Corrupted => {
+                t!(format!("{:?} <-- message corrupted", msg).red());
+            }
+        }
+    }
 
     fn log_mode(&self) -> &LogMode;
 }
