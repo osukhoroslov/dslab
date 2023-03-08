3c81f6d9948c856f36f9808bf93a6e40e4c1c1f4
AnthonyUdovichenko <antonyud2000@yandex.ru>
2023-03-08 21:09:52 +0300

    Add duplicate function to McEvent

--- crates/dslab-mp/src/mc/events.rs
+++ crates/dslab-mp/src/mc/events.rs
@@ -3,6 +3,20 @@ use serde::Serialize;
 use crate::message::Message;
 
 #[derive(Serialize, Clone)]
+pub enum DeliveryOptions {
+    /// Message will be received exactly once without corruption
+    NoFailures,
+    /// Message will not be received
+    Dropped,
+    /// Message delivery may be subject to some failures
+    PossibleFailures {
+        can_be_dropped: bool,
+        max_dupl_count: u32,
+        can_be_corrupted: bool,
+    },
+}
+
+#[derive(Serialize, Clone)]
 pub enum McEvent {
     MessageReceived {
         msg: Message,
@@ -20,16 +34,30 @@ pub enum McEvent {
     },
 }
 
-#[derive(Serialize, Clone)]
-pub enum DeliveryOptions {
-    /// Message will be received exactly once without corruption
-    NoFailures,
-    /// Message will not be received
-    Dropped,
-    /// Message delivery may be subject to some failures
-    PossibleFailures {
-        can_be_dropped: bool,
-        max_dupl_count: u32,
-        can_be_corrupted: bool,
-    },
+impl McEvent {
+    pub fn duplicate(&self) -> Option<McEvent> {
+        match self {
+            McEvent::MessageReceived {
+                msg,
+                src,
+                dest,
+                options:
+                    DeliveryOptions::PossibleFailures {
+                        can_be_dropped,
+                        max_dupl_count,
+                        can_be_corrupted,
+                    },
+            } => Some(McEvent::MessageReceived {
+                msg: msg.clone(),
+                src: src.clone(),
+                dest: dest.clone(),
+                options: DeliveryOptions::PossibleFailures {
+                    can_be_dropped: *can_be_dropped,
+                    max_dupl_count: max_dupl_count - 1,
+                    can_be_corrupted: *can_be_corrupted,
+                },
+            }),
+            _ => None,
+        }
+    }
 }
