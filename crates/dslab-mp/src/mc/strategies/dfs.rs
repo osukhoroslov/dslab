665ad134d1f2a64182659906ec37d5af87c3c846
Konstantin Amelichev <kostya.amelichev@gmail.com>
2023-03-10 02:15:39 +0400

    update dfs

--- crates/dslab-mp/src/mc/strategies/dfs.rs
+++ crates/dslab-mp/src/mc/strategies/dfs.rs
@@ -1,6 +1,4 @@
 use std::collections::HashSet;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
 
 
 use crate::mc::strategy::{LogMode, McSummary, Strategy};
@@ -46,11 +44,11 @@ impl Dfs {
         } else if let Err(err) = (self.invariant)(&state) {
             // Invariant is broken
             Some(Err(err))
-        } else if let Some(status) == (self.goal)(&state) {
+        } else if let Some(status) = (self.goal)(&state) {
             // Reached final state of the system
             self.update_summary(status);
             Some(Ok(()))
-        } else if let Some(status) == (self.prune)(&state) {
+        } else if let Some(status) = (self.prune)(&state) {
             // Execution branch is pruned
             self.update_summary(status);
             Some(Ok(()))
