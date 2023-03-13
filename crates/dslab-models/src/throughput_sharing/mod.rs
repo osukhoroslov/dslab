794f2279efc0da2246a9d155d63bfc804ef0c42f
Tagir Kuskarov <kuskarovtf@mail.ru>
2023-03-13 00:00:52 +0000

    Rearrange imports

--- crates/dslab-models/src/throughput_sharing/mod.rs
+++ crates/dslab-models/src/throughput_sharing/mod.rs
@@ -3,7 +3,7 @@
 mod fair_fast;
 mod fair_slow;
 mod model;
-pub mod throughput_factor;
+mod throughput_factor;
 
 #[cfg(test)]
 mod tests;
@@ -11,3 +11,7 @@ mod tests;
 pub use fair_fast::FairThroughputSharingModel;
 pub use fair_slow::SlowFairThroughputSharingModel;
 pub use model::{make_constant_throughput_function, ThroughputFunction, ThroughputSharingModel};
+pub use throughput_factor::{
+    make_uniform_throughput_factor_function, ConstantThroughputFactorFunction, EmpiricalThroughputFactorFunction,
+    RandomizedThroughputFactorFunction, ThroughputFactorFunction, WeightedThroughputFactor,
+};
