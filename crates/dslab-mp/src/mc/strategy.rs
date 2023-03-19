a1cdcbc2ca46747cdda266676c3f65ca7fc51567
AnthonyUdovichenko <antonyud2000@yandex.ru>
2023-03-19 20:12:25 +0300

    Apply rustfmt

--- crates/dslab-mp/src/mc/strategy.rs
+++ crates/dslab-mp/src/mc/strategy.rs
@@ -1,5 +1,5 @@
-use std::collections::{HashMap, HashSet};
 use std::collections::hash_map::DefaultHasher;
+use std::collections::{HashMap, HashSet};
 use std::hash::{Hash, Hasher};
 
 use colored::*;
