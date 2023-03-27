2067468f58b81e83a66aebb494b20650da68ffab
Tagir Kuskarov <kuskarovtf@mail.ru>
2023-03-27 15:51:42 +0000

    Merge storage-disk and storage-shared-disk

--- examples/storage-disk/src/main.rs
+++ examples/storage-disk/src/main.rs
@@ -24,7 +24,7 @@ const SEED: u64 = 16;
 const DISK_NAME: &str = "Disk";
 const USER_NAME: &str = "User";
 
-const DISK_CAPACITY: u64 = 5;
+const DISK_CAPACITY: u64 = 1000;
 const DISK_READ_BW: f64 = 100.;
 const DISK_WRITE_BW: f64 = 100.;
 
@@ -35,7 +35,13 @@ struct User {
 }
 
 #[derive(Clone, Serialize)]
-struct Start {}
+struct Start1 {}
+
+#[derive(Clone, Serialize)]
+struct Start2 {}
+
+#[derive(Clone, Serialize)]
+struct Ticker {}
 
 impl User {
     fn new(disk: Rc<RefCell<Disk>>, ctx: SimulationContext) -> Self {
@@ -50,22 +56,53 @@ impl User {
 impl EventHandler for User {
     fn on(&mut self, event: Event) {
         cast!(match event.data {
-            Start {} => {
-                log_debug!(self.ctx, "Test #0: Reading 3 bytes... should be OK");
-                self.requests.insert(self.disk.borrow_mut().read(3, self.ctx.id()), 0);
+            Start1 {} => {
+                self.ctx.emit_self(Ticker {}, 0.);
+            }
+            Ticker {} => {
+                let time = self.ctx.time();
+                if time.eq(&0.) {
+                    log_debug!(self.ctx, "Test #1.0: Single 100 byte read, expected to end at t=1");
+                    self.requests.insert(self.disk.borrow_mut().read(100, self.ctx.id()), 0);
+                } else if time.eq(&1.) {
+                    log_debug!(self.ctx, "Test #1.1: Two 50 byte reads, expected to end at t=2");
+                    self.requests.insert(self.disk.borrow_mut().read(50, self.ctx.id()), 1);
+                    self.requests.insert(self.disk.borrow_mut().read(50, self.ctx.id()), 1);
+                } else if time.eq(&2.) {
+                    log_debug!(
+                        self.ctx,
+                        "Test #1.2: Starting 1st 200 byte read, expected to end at t=5"
+                    );
+                    self.requests.insert(self.disk.borrow_mut().read(200, self.ctx.id()), 2);
+                } else if time.eq(&3.) {
+                    log_debug!(
+                        self.ctx,
+                        "Test #1.2: Starting 2nd 200 byte read, expected to end at t=6"
+                    );
+                    self.requests.insert(self.disk.borrow_mut().read(200, self.ctx.id()), 2);
+                    return;
+                }
+                self.ctx.emit_self(Ticker {}, 1.);
+            }
+            Start2 {} => {
+                log_debug!(self.ctx, "Test #2.0: Reading 600 bytes... should be OK");
+                self.requests.insert(self.disk.borrow_mut().read(600, self.ctx.id()), 0);
 
-                log_debug!(self.ctx, "Test #1: Then trying to read 6 bytes... should fail");
-                self.requests.insert(self.disk.borrow_mut().read(6, self.ctx.id()), 1);
+                log_debug!(self.ctx, "Test #2.1: Then trying to read 1200 bytes... should fail");
+                self.requests
+                    .insert(self.disk.borrow_mut().read(1200, self.ctx.id()), 1);
 
                 log_debug!(self.ctx, "Used space: {}", self.disk.borrow().used_space());
 
-                log_debug!(self.ctx, "Test #2: Writing 4 bytes... should be OK");
-                self.requests.insert(self.disk.borrow_mut().write(4, self.ctx.id()), 2);
+                log_debug!(self.ctx, "Test #2.2: Writing 800 bytes... should be OK");
+                self.requests
+                    .insert(self.disk.borrow_mut().write(800, self.ctx.id()), 2);
 
                 log_debug!(self.ctx, "Used space: {}", self.disk.borrow().used_space());
 
-                log_debug!(self.ctx, "Test #3: Writing 2 more bytes... should fail");
-                self.requests.insert(self.disk.borrow_mut().write(2, self.ctx.id()), 3);
+                log_debug!(self.ctx, "Test #2.3: Writing 400 more bytes... should fail");
+                self.requests
+                    .insert(self.disk.borrow_mut().write(400, self.ctx.id()), 3);
 
                 log_debug!(self.ctx, "Used space: {}", self.disk.borrow().used_space());
             }
@@ -129,13 +166,24 @@ fn main() {
         boxed!(make_uniform_factor_fn(0.9, 1.1)),
         sim.create_context(DISK_NAME),
     )));
+    sim.add_handler(DISK_NAME, disk.clone());
 
-    let user = rc!(refcell!(User::new(disk, sim.create_context(USER_NAME))));
+    let user = rc!(refcell!(User::new(disk.clone(), sim.create_context(USER_NAME))));
     let user_id = sim.add_handler(USER_NAME, user);
 
     let root = sim.create_context("root");
     root.emit_now(Start {}, user_id);
 
+    println!("Starting test pack 1...");
+    root.emit_now(Start1 {}, user_id);
+    sim.step_until_no_events();
+
+    println!("Clearing disk after test pack 1...");
+    let used_space = disk.borrow().used_space();
+    assert!(disk.borrow_mut().mark_free(used_space).is_ok());
+
+    println!("Starting test pack 2...");
+    root.emit_now(Start2 {}, user_id);
     sim.step_until_no_events();
 
     println!("Finish");
