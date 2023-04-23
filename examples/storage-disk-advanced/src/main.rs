8df377c5894b03cc7899201d4b23cf78109f5174
Tagir Kuskarov <kuskarovtf@mail.ru>
2023-04-23 02:02:43 +0000

    Split disk example to simple and advanced

---
+++ examples/storage-disk-advanced/src/main.rs
@@ -0,0 +1,179 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::io::Write;
+use std::rc::Rc;
+
+use env_logger::Builder;
+use serde::Serialize;
+use sugars::{boxed, rc, refcell};
+
+use dslab_core::cast;
+use dslab_core::context::SimulationContext;
+use dslab_core::event::Event;
+use dslab_core::handler::EventHandler;
+use dslab_core::log_info;
+use dslab_core::simulation::Simulation;
+
+use dslab_models::throughput_sharing::{make_constant_throughput_fn, make_uniform_factor_fn, EmpiricalFactorFn};
+use dslab_storage::disk::Disk;
+use dslab_storage::events::{DataReadCompleted, DataReadFailed, DataWriteCompleted, DataWriteFailed};
+use dslab_storage::storage::Storage;
+
+const SEED: u64 = 16;
+
+const SIMPLE_DISK_NAME: &str = "SimpleDisk";
+const ADVANCED_DISK_NAME: &str = "AdvancedDisk";
+
+const SIMPLE_USER_NAME: &str = "SimpleUser";
+const ADVANCED_USER_NAME: &str = "AdvancedUser";
+
+const DISK_CAPACITY: u64 = 1000;
+const DISK_READ_BW: f64 = 100.;
+const DISK_WRITE_BW: f64 = 100.;
+
+const READ_ITERATIONS: u64 = 100;
+const WRITE_ITERATIONS: u64 = 100;
+
+struct User {
+    disk: Rc<RefCell<Disk>>,
+    requests: HashMap<u64, u64>, // request_id -> test case
+    ctx: SimulationContext,
+    start_time: f64,
+}
+
+#[derive(Clone, Serialize)]
+struct Start {}
+
+#[derive(Clone, Serialize)]
+struct Ticker {}
+
+impl User {
+    fn new(disk: Rc<RefCell<Disk>>, ctx: SimulationContext) -> Self {
+        Self {
+            disk,
+            requests: HashMap::new(),
+            ctx,
+            start_time: 0.,
+        }
+    }
+}
+
+impl EventHandler for User {
+    fn on(&mut self, event: Event) {
+        cast!(match event.data {
+            Start {} => {
+                self.start_time = self.ctx.time();
+                for i in 1..READ_ITERATIONS {
+                    self.requests.insert(self.disk.borrow_mut().read(10, self.ctx.id()), i);
+                }
+                for i in 1..WRITE_ITERATIONS {
+                    self.requests
+                        .insert(self.disk.borrow_mut().write(10, self.ctx.id()), READ_ITERATIONS + i);
+                }
+            }
+            DataReadCompleted { request_id, size: _ } => {
+                log_info!(
+                    self.ctx,
+                    "Read iteration #{} completed. Elapsed time = {}",
+                    self.requests[&request_id],
+                    self.ctx.time() - self.start_time,
+                );
+            }
+            DataReadFailed { request_id, error } => {
+                log_info!(
+                    self.ctx,
+                    "Read iteration #{} failed. Error: {}",
+                    self.requests[&request_id],
+                    error
+                );
+            }
+            DataWriteCompleted { request_id, size: _ } => {
+                log_info!(
+                    self.ctx,
+                    "Write iteration #{} completed. Elapsed time = {}",
+                    self.requests[&request_id] - READ_ITERATIONS,
+                    self.ctx.time() - self.start_time,
+                );
+            }
+            DataWriteFailed { request_id, error } => {
+                log_info!(
+                    self.ctx,
+                    "Write iteration #{} failed. Error: {}",
+                    self.requests[&request_id],
+                    error
+                );
+            }
+        })
+    }
+}
+
+fn main() {
+    Builder::from_default_env()
+        .format(|buf, record| writeln!(buf, "{}", record.args()))
+        .init();
+
+    let mut sim = Simulation::new(SEED);
+    let root = sim.create_context("root");
+
+    let simple_disk = rc!(refcell!(Disk::new_simple(
+        DISK_CAPACITY,
+        DISK_READ_BW,
+        DISK_WRITE_BW,
+        sim.create_context(SIMPLE_DISK_NAME)
+    )));
+    sim.add_handler(SIMPLE_DISK_NAME, simple_disk.clone());
+
+    println!("Starting simple user...");
+
+    let simple_user = rc!(refcell!(User::new(
+        simple_disk.clone(),
+        sim.create_context(SIMPLE_USER_NAME)
+    )));
+    root.emit_now(Start {}, sim.add_handler(SIMPLE_USER_NAME, simple_user));
+
+    // Elapsed times in logs will be equal for all activities.
+    sim.step_until_no_events();
+
+    println!("Finished simple user");
+
+    let advanced_disk = rc!(refcell!(Disk::new(
+        DISK_CAPACITY,
+        //
+        // Using the constant throughput function for read operations,
+        // so total throughput will not change during the simulation.
+        make_constant_throughput_fn(DISK_READ_BW),
+        //
+        // Using custom throughput function for write operations,
+        // so total throughput will depend on activities count `n` as follows.
+        boxed!(|n| {
+            if n < 4 {
+                DISK_WRITE_BW
+            } else {
+                DISK_WRITE_BW / 2.
+            }
+        }),
+        //
+        // Using the uniformly randomized factor function for read operations,
+        // so read bandwidth will be multiplied by random factor from 0.9 to 1.1.
+        boxed!(make_uniform_factor_fn(0.9, 1.1)),
+        //
+        // Using the empirical factor function for write operations,
+        // so write bandwidth will be multiplied by factor generated from distribution weighted with given weights.
+        boxed!(EmpiricalFactorFn::new(&[(0.8, 3), (0.9, 10), (1., 31), (1.1, 15), (1.2, 5), (1.3, 6)]).unwrap()),
+        sim.create_context(ADVANCED_DISK_NAME),
+    )));
+    sim.add_handler(ADVANCED_DISK_NAME, advanced_disk.clone());
+
+    println!("Starting advanced user...");
+
+    let advanced_user = rc!(refcell!(User::new(
+        advanced_disk.clone(),
+        sim.create_context(ADVANCED_USER_NAME)
+    )));
+    root.emit_now(Start {}, sim.add_handler(ADVANCED_USER_NAME, advanced_user));
+
+    // Elapsed times in logs will differ in same activities.
+    sim.step_until_no_events();
+
+    println!("Finished advanced user");
+}
--- examples/storage-disk/src/main.rs
+++ examples/storage-disk/src/main.rs
@@ -5,7 +5,7 @@ use std::rc::Rc;
 
 use env_logger::Builder;
 use serde::Serialize;
-use sugars::{boxed, rc, refcell};
+use sugars::{rc, refcell};
 
 use dslab_core::cast;
 use dslab_core::context::SimulationContext;
@@ -14,7 +14,6 @@ use dslab_core::handler::EventHandler;
 use dslab_core::log_debug;
 use dslab_core::simulation::Simulation;
 
-use dslab_models::throughput_sharing::{make_constant_throughput_fn, make_uniform_factor_fn, EmpiricalFactorFn};
 use dslab_storage::disk::Disk;
 use dslab_storage::events::{DataReadCompleted, DataReadFailed, DataWriteCompleted, DataWriteFailed};
 use dslab_storage::storage::Storage;
@@ -24,7 +23,7 @@ const SEED: u64 = 16;
 const DISK_NAME: &str = "Disk";
 const USER_NAME: &str = "User";
 
-const DISK_CAPACITY: u64 = 1000;
+const DISK_CAPACITY: u64 = 200;
 const DISK_READ_BW: f64 = 100.;
 const DISK_WRITE_BW: f64 = 100.;
 
@@ -35,10 +34,7 @@ struct User {
 }
 
 #[derive(Clone, Serialize)]
-struct Start1 {}
-
-#[derive(Clone, Serialize)]
-struct Start2 {}
+struct Start {}
 
 #[derive(Clone, Serialize)]
 struct Ticker {}
@@ -56,60 +52,36 @@ impl User {
 impl EventHandler for User {
     fn on(&mut self, event: Event) {
         cast!(match event.data {
-            Start1 {} => {
+            Start {} => {
                 self.ctx.emit_self(Ticker {}, 0.);
             }
             Ticker {} => {
                 let time = self.ctx.time();
                 if time.eq(&0.) {
-                    log_debug!(self.ctx, "Test #1.0: Single 100 byte read, expected to end at t=1");
+                    log_debug!(self.ctx, "Step #0: Single 100 byte read, expected to end at t=1");
                     self.requests.insert(self.disk.borrow_mut().read(100, self.ctx.id()), 0);
                 } else if time.eq(&1.) {
-                    log_debug!(self.ctx, "Test #1.1: Two 50 byte reads, expected to end at t=2");
+                    log_debug!(self.ctx, "Step #1: Two 50 byte reads, expected to end at t=2");
                     self.requests.insert(self.disk.borrow_mut().read(50, self.ctx.id()), 1);
                     self.requests.insert(self.disk.borrow_mut().read(50, self.ctx.id()), 1);
                 } else if time.eq(&2.) {
-                    log_debug!(
-                        self.ctx,
-                        "Test #1.2: Starting 1st 200 byte read, expected to end at t=5"
-                    );
+                    log_debug!(self.ctx, "Step #2: Starting 1st 200 byte read, expected to end at t=5");
                     self.requests.insert(self.disk.borrow_mut().read(200, self.ctx.id()), 2);
                 } else if time.eq(&3.) {
-                    log_debug!(
-                        self.ctx,
-                        "Test #1.2: Starting 2nd 200 byte read, expected to end at t=6"
-                    );
-                    self.requests.insert(self.disk.borrow_mut().read(200, self.ctx.id()), 2);
+                    log_debug!(self.ctx, "Step #3: Starting 2nd 200 byte read, expected to end at t=6");
+                    self.requests.insert(self.disk.borrow_mut().read(200, self.ctx.id()), 3);
+                } else if time.eq(&4.) {
+                    log_debug!(self.ctx, "Step #4: Trying to read 201 bytes... should fail");
+                    self.requests.insert(self.disk.borrow_mut().read(201, self.ctx.id()), 4);
+                } else if time.eq(&6.) {
                     return;
                 }
                 self.ctx.emit_self(Ticker {}, 1.);
             }
-            Start2 {} => {
-                log_debug!(self.ctx, "Test #2.0: Reading 600 bytes... should be OK");
-                self.requests.insert(self.disk.borrow_mut().read(600, self.ctx.id()), 0);
-
-                log_debug!(self.ctx, "Test #2.1: Then trying to read 1200 bytes... should fail");
-                self.requests
-                    .insert(self.disk.borrow_mut().read(1200, self.ctx.id()), 1);
-
-                log_debug!(self.ctx, "Used space: {}", self.disk.borrow().used_space());
-
-                log_debug!(self.ctx, "Test #2.2: Writing 800 bytes... should be OK");
-                self.requests
-                    .insert(self.disk.borrow_mut().write(800, self.ctx.id()), 2);
-
-                log_debug!(self.ctx, "Used space: {}", self.disk.borrow().used_space());
-
-                log_debug!(self.ctx, "Test #2.3: Writing 400 more bytes... should fail");
-                self.requests
-                    .insert(self.disk.borrow_mut().write(400, self.ctx.id()), 3);
-
-                log_debug!(self.ctx, "Used space: {}", self.disk.borrow().used_space());
-            }
             DataReadCompleted { request_id, size } => {
                 log_debug!(
                     self.ctx,
-                    "Test #{}: Completed reading {} bytes from disk",
+                    "Step #{}: Completed reading {} bytes from disk",
                     self.requests[&request_id],
                     size
                 );
@@ -117,7 +89,7 @@ impl EventHandler for User {
             DataReadFailed { request_id, error } => {
                 log_debug!(
                     self.ctx,
-                    "Test #{}: Reading failed. Error: {}",
+                    "Step #{}: Reading failed. Error: {}",
                     self.requests[&request_id],
                     error
                 );
@@ -125,7 +97,7 @@ impl EventHandler for User {
             DataWriteCompleted { request_id, size } => {
                 log_debug!(
                     self.ctx,
-                    "Test #{}: Completed writing {} bytes to disk",
+                    "Step #{}: Completed writing {} bytes to disk",
                     self.requests[&request_id],
                     size
                 );
@@ -133,7 +105,7 @@ impl EventHandler for User {
             DataWriteFailed { request_id, error } => {
                 log_debug!(
                     self.ctx,
-                    "Test #{}: Writing failed. Error: {}",
+                    "Step #{}: Writing failed. Error: {}",
                     self.requests[&request_id],
                     error
                 );
@@ -143,47 +115,25 @@ impl EventHandler for User {
 }
 
 fn main() {
-    println!("Starting...");
-
     Builder::from_default_env()
         .format(|buf, record| writeln!(buf, "{}", record.args()))
         .init();
 
     let mut sim = Simulation::new(SEED);
 
-    // Creating empirical factor function with weighted points distribution
-    let model = EmpiricalFactorFn::new(&[(0.8, 3), (0.9, 10), (1., 31), (1.1, 15), (1.2, 5), (1.3, 6)]);
-    assert!(model.is_ok());
-
-    let disk = rc!(refcell!(Disk::new(
+    let disk = rc!(refcell!(Disk::new_simple(
         DISK_CAPACITY,
-        // Using the constant throughput function for read operations
-        make_constant_throughput_fn(DISK_READ_BW),
-        make_constant_throughput_fn(DISK_WRITE_BW),
-        // Using the created factor function for read operations
-        boxed!(model.unwrap()),
-        // Using the randomized uniform factor function for write operations
-        boxed!(make_uniform_factor_fn(0.9, 1.1)),
+        DISK_READ_BW,
+        DISK_WRITE_BW,
         sim.create_context(DISK_NAME),
     )));
     sim.add_handler(DISK_NAME, disk.clone());
 
     let user = rc!(refcell!(User::new(disk.clone(), sim.create_context(USER_NAME))));
-    let user_id = sim.add_handler(USER_NAME, user);
-
     let root = sim.create_context("root");
 
-    println!("Starting test pack 1...");
-    root.emit_now(Start1 {}, user_id);
+    println!("Starting...");
+    root.emit_now(Start {}, sim.add_handler(USER_NAME, user));
     sim.step_until_no_events();
-
-    println!("Clearing disk after test pack 1...");
-    let used_space = disk.borrow().used_space();
-    assert!(disk.borrow_mut().mark_free(used_space).is_ok());
-
-    println!("Starting test pack 2...");
-    root.emit_now(Start2 {}, user_id);
-    sim.step_until_no_events();
-
     println!("Finish");
 }
