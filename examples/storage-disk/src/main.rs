8e45798fb886447a6a8d6a32978c6771de3cf583
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2023-05-19 18:01:28 +0300

    improved storage-disk example

--- examples/storage-disk/src/main.rs
+++ examples/storage-disk/src/main.rs
@@ -4,15 +4,16 @@ use std::io::Write;
 use std::rc::Rc;
 
 use env_logger::Builder;
+use log::LevelFilter;
 use serde::Serialize;
 use sugars::{rc, refcell};
 
-use dslab_core::cast;
 use dslab_core::context::SimulationContext;
 use dslab_core::event::Event;
 use dslab_core::handler::EventHandler;
 use dslab_core::log_info;
 use dslab_core::simulation::Simulation;
+use dslab_core::{cast, Id};
 
 use dslab_storage::disk::{Disk, DiskBuilder};
 use dslab_storage::events::{DataReadCompleted, DataReadFailed, DataWriteCompleted, DataWriteFailed};
@@ -27,14 +28,38 @@ const DISK_CAPACITY: u64 = 300;
 const DISK_READ_BW: f64 = 100.;
 const DISK_WRITE_BW: f64 = 200.;
 
+fn main() {
+    // setup logging
+    Builder::new()
+        .filter_level(LevelFilter::Info)
+        .parse_default_env()
+        .format(|buf, record| writeln!(buf, "{}", record.args()))
+        .init();
+
+    // build simulation with a disk and a client
+    let mut sim = Simulation::new(SEED);
+
+    let disk = rc!(refcell!(DiskBuilder::simple(
+        DISK_CAPACITY,
+        DISK_READ_BW,
+        DISK_WRITE_BW,
+    )
+    .build(sim.create_context(DISK_NAME))));
+    sim.add_handler(DISK_NAME, disk.clone());
+
+    let client = rc!(refcell!(Client::new(disk, sim.create_context(CLIENT_NAME))));
+    sim.add_handler(CLIENT_NAME, client.clone());
+
+    // run the simulation
+    client.borrow_mut().start();
+    sim.step_until_no_events();
+}
+
 struct Client {
     disk: Rc<RefCell<Disk>>,
     requests: HashMap<u64, u64>, // request_id -> test case
     ctx: SimulationContext,
 }
-
-#[derive(Clone, Serialize)]
-struct Start {}
 
 #[derive(Clone, Serialize)]
 struct Ticker {}
@@ -46,6 +71,14 @@ impl Client {
             requests: HashMap::new(),
             ctx,
         }
+    }
+
+    fn id(&self) -> Id {
+        self.ctx.id()
+    }
+
+    fn start(&mut self) {
+        self.ctx.emit_self(Ticker {}, 0.);
     }
 
     fn print_disk_info(&self) {
@@ -63,49 +96,55 @@ impl Client {
 impl EventHandler for Client {
     fn on(&mut self, event: Event) {
         cast!(match event.data {
-            Start {} => {
-                self.ctx.emit_self(Ticker {}, 0.);
-            }
             Ticker {} => {
                 let time = self.ctx.time();
                 let step = time as u64;
-                if time == 0. {
-                    self.print_disk_info();
-                    log_info!(self.ctx, "Step #0: Single 100 byte write, expected to end at t=1");
-                    self.requests
-                        .insert(self.disk.borrow_mut().write(100, self.ctx.id()), step);
-                } else if time.eq(&1.) {
-                    log_info!(self.ctx, "Step #1: Two 50 byte writes, expected to end at t=2");
-                    self.requests
-                        .insert(self.disk.borrow_mut().write(50, self.ctx.id()), step);
-                    self.requests
-                        .insert(self.disk.borrow_mut().write(50, self.ctx.id()), step);
-                } else if time.eq(&2.) {
-                    log_info!(self.ctx, "Step #2: Starting 1st 200 byte read, expected to end at t=5");
-                    self.requests
-                        .insert(self.disk.borrow_mut().read(200, self.ctx.id()), step);
-                } else if time.eq(&3.) {
-                    log_info!(self.ctx, "Step #3: Starting 2nd 200 byte read, expected to end at t=6");
-                    self.requests
-                        .insert(self.disk.borrow_mut().read(200, self.ctx.id()), step);
-                } else if time.eq(&4.) {
-                    log_info!(self.ctx, "Step #4: Trying to write 101 bytes... should fail");
-                    self.requests
-                        .insert(self.disk.borrow_mut().write(101, self.ctx.id()), step);
-                } else if time.eq(&5.) {
-                    log_info!(self.ctx, "Step #5: Trying to read 301 bytes... should fail");
-                    self.requests
-                        .insert(self.disk.borrow_mut().read(301, self.ctx.id()), step);
-                } else if time.eq(&6.) {
-                    log_info!(
-                        self.ctx,
-                        "Step #6: Cleaning some space and trying to write once more... now success"
-                    );
-                    self.disk.borrow_mut().mark_free(1).unwrap();
-                    self.print_disk_info();
-                    self.requests
-                        .insert(self.disk.borrow_mut().write(101, self.ctx.id()), step);
-                    return;
+                match step {
+                    0 => {
+                        self.print_disk_info();
+                        log_info!(self.ctx, "Step #0: Single 100 byte write, expected to end at t=1");
+                        let req = self.disk.borrow_mut().write(100, self.id());
+                        self.requests.insert(req, step);
+                    }
+                    1 => {
+                        log_info!(self.ctx, "Step #1: Two 50 byte writes, expected to end at t=2");
+                        let req1 = self.disk.borrow_mut().write(50, self.id());
+                        let req2 = self.disk.borrow_mut().write(50, self.id());
+                        self.requests.insert(req1, step);
+                        self.requests.insert(req2, step);
+                    }
+                    2 => {
+                        log_info!(self.ctx, "Step #2: Starting 1st 200 byte read, expected to end at t=5");
+                        let req = self.disk.borrow_mut().read(200, self.id());
+                        self.requests.insert(req, step);
+                    }
+                    3 => {
+                        log_info!(self.ctx, "Step #3: Starting 2nd 200 byte read, expected to end at t=6");
+                        let req = self.disk.borrow_mut().read(200, self.id());
+                        self.requests.insert(req, step);
+                    }
+                    4 => {
+                        log_info!(self.ctx, "Step #4: Trying to write 101 bytes... should fail");
+                        let req = self.disk.borrow_mut().write(101, self.id());
+                        self.requests.insert(req, step);
+                    }
+                    5 => {
+                        log_info!(self.ctx, "Step #5: Trying to read 301 bytes... should fail");
+                        let req = self.disk.borrow_mut().read(301, self.id());
+                        self.requests.insert(req, step);
+                    }
+                    6 => {
+                        log_info!(
+                            self.ctx,
+                            "Step #6: Freeing space and trying to write once more... now success"
+                        );
+                        self.disk.borrow_mut().mark_free(1).unwrap();
+                        self.print_disk_info();
+                        let req = self.disk.borrow_mut().write(101, self.id());
+                        self.requests.insert(req, step);
+                        return;
+                    }
+                    _ => {}
                 }
                 self.ctx.emit_self(Ticker {}, 1.);
             }
@@ -145,28 +184,4 @@ impl EventHandler for Client {
             }
         })
     }
-}
-
-fn main() {
-    Builder::from_default_env()
-        .format(|buf, record| writeln!(buf, "{}", record.args()))
-        .init();
-
-    let mut sim = Simulation::new(SEED);
-
-    let disk = rc!(refcell!(DiskBuilder::simple(
-        DISK_CAPACITY,
-        DISK_READ_BW,
-        DISK_WRITE_BW,
-    )
-    .build(sim.create_context(DISK_NAME))));
-    sim.add_handler(DISK_NAME, disk.clone());
-
-    let user = rc!(refcell!(Client::new(disk, sim.create_context(CLIENT_NAME))));
-    let root = sim.create_context("root");
-
-    println!("Starting...");
-    root.emit_now(Start {}, sim.add_handler(CLIENT_NAME, user));
-    sim.step_until_no_events();
-    println!("Finished");
 }
