7135b9539001cf4d4c5d784734fc8b5301b6925a
Artem Makogon <artem.makogon.nsk@mail.ru>
2023-05-22 16:00:18 +0300

    waker fix

--- crates/dslab-core/src/async_core/waker.rs
+++ crates/dslab-core/src/async_core/waker.rs
@@ -1,11 +1,13 @@
-//! custom creating wakers to avoid Send+Sync dependency
+//! Custom task waker creation to avoid Send+Sync dependency.
+//!
+//! Original implementation from crate futures:
+//! https://github.com/rust-lang/futures-rs/blob/master/futures-task/src/waker.rs
 
 use std::{
     sync::Arc,
     task::{RawWaker, RawWakerVTable, Waker},
 };
 
-use core::mem;
 use core::mem::ManuallyDrop;
 
 use futures::task::WakerRef;
@@ -39,26 +41,12 @@ pub(super) fn waker_vtable<W: CustomWake>() -> &'static RawWakerVTable {
     )
 }
 
-/// Creates a [`Waker`] from an `Arc<impl CustomWake>`.
-///
-/// The returned [`Waker`] will call
-/// [`CustomWake.wake()`](CustomWake::wake) if awoken.
-#[allow(dead_code)]
-pub(super) fn waker<W>(wake: Arc<W>) -> Waker
-where
-    W: CustomWake + 'static,
-{
-    let ptr = Arc::into_raw(wake).cast::<()>();
-
-    unsafe { Waker::from_raw(RawWaker::new(ptr, waker_vtable::<W>())) }
-}
-
 #[allow(clippy::redundant_clone)] // The clone here isn't actually redundant.
 unsafe fn increase_refcount<T: CustomWake>(data: *const ()) {
     // Retain Arc, but don't touch refcount by wrapping in ManuallyDrop
-    let arc = mem::ManuallyDrop::new(Arc::<T>::from_raw(data.cast::<T>()));
+    let arc = ManuallyDrop::new(Arc::<T>::from_raw(data.cast::<T>()));
     // Now increase refcount, but don't drop new refcount either
-    let _arc_clone: mem::ManuallyDrop<_> = arc.clone();
+    let _arc_clone: ManuallyDrop<_> = arc.clone();
 }
 
 // used by `waker_ref`
@@ -75,7 +63,7 @@ unsafe fn wake_arc_raw<T: CustomWake>(data: *const ()) {
 // used by `waker_ref`
 unsafe fn wake_by_ref_arc_raw<T: CustomWake>(data: *const ()) {
     // Retain Arc, but don't touch refcount by wrapping in ManuallyDrop
-    let arc = mem::ManuallyDrop::new(Arc::<T>::from_raw(data.cast::<T>()));
+    let arc = ManuallyDrop::new(Arc::<T>::from_raw(data.cast::<T>()));
     CustomWake::wake_by_ref(&arc);
 }
 
