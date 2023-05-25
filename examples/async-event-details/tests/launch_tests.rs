0d9d6dfa01d485af3277669c78beeb0d7431e1e4
Artem Makogon <artem.makogon.nsk@mail.ru>
2023-05-25 22:04:48 +0300

    more integration tests

---
+++ examples/async-event-details/tests/launch_tests.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+
+fn launch(args: &[&str]) {
+    let output = Command::new("cargo")
+        .args(&["run", "--"])
+        .args(args)
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    println!("OUTPUT: {}", stdout);
+    println!("STDERR: {}", stderr);
+
+    assert!(output.status.success());
+    assert!(!stderr.to_lowercase().contains("error"));
+}
+
+#[test]
+fn test_launching() {
+    launch(&["--tasks-count", "1000"]);
+}
