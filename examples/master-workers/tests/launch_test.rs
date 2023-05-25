5b45623bd0707f9ee2dbd70182788f5386dfdb9e
Artem Makogon <artem.makogon.nsk@mail.ru>
2023-05-25 21:41:22 +0300

    add test

---
+++ examples/master-workers/tests/launch_test.rs
@@ -0,0 +1,17 @@
+use std::process::Command;
+
+#[test]
+fn test_launching() {
+    let output = Command::new("cargo")
+        .args(&["run", "--", "--task-count", "2000"])
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    println!("OUTPUT: {}", stdout);
+    println!("STDERR: {}", stderr);
+
+    assert!(output.status.success());
+    assert!(!stderr.to_lowercase().contains("error"));
+}
