527a454d6083f165edd66681f7f706b69e7b3570
AnthonyUdovichenko <antonyud2000@yandex.ru>
2023-06-25 21:08:31 +0300

    Add McError docs

--- crates/dslab-mp/src/mc/error.rs
+++ crates/dslab-mp/src/mc/error.rs
@@ -1,5 +1,8 @@
+//! Implementation of model checking error struct
+
 use crate::logger::LogEntry;
 
+/// Model checking error struct
 #[derive(Debug, Default, PartialEq)]
 pub struct McError {
     message: String,
@@ -7,16 +10,19 @@ pub struct McError {
 }
 
 impl McError {
+    /// Create new model checking error
     pub fn new(message: String, trace: Vec<LogEntry>) -> Self {
         Self { message, trace }
     }
 }
 
 impl McError {
+    /// Get model checking error message
     pub fn message(&self) -> String {
         self.message.clone()
     }
 
+    /// Get trace which led system to the error
     pub fn trace(&self) -> &Vec<LogEntry> {
         &self.trace
     }
