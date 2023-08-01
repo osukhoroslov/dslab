f595e56e3a0020031d2a05f0fe0568a937d0ab38
Maksim Gorokhovskii <magor1744@gmail.com>
2023-08-02 01:09:40 +0300

    more tests

--- crates/dslab-network/tests/tests.rs
+++ crates/dslab-network/tests/tests.rs
@@ -184,3 +184,114 @@ fn test_links(
         EPSILON,
     );
 }
+
+#[rstest]
+fn test_triangle(#[values(RoutingImpl::Dijkstra, RoutingImpl::FloydWarshall)] routing: RoutingImpl) {
+    let mut sim = Simulation::new(123);
+
+    let network_model = Box::new(TopologyAwareNetworkModel::new());
+    let routing: Box<dyn RoutingAlgorithm> = match routing {
+        RoutingImpl::Dijkstra => Box::new(ShortestPathDijkstra::default()),
+        RoutingImpl::FloydWarshall => Box::new(ShortestPathFloydWarshall::default()),
+    };
+    let mut network = Network::with_routing(routing, network_model, sim.create_context("net"));
+
+    network.add_node("host1", Box::new(ConstantBandwidthNetworkModel::new(100.0, 0.0)));
+    network.add_node("host2", Box::new(ConstantBandwidthNetworkModel::new(100.0, 0.0)));
+    network.add_node("host3", Box::new(ConstantBandwidthNetworkModel::new(100.0, 0.0)));
+
+    network.add_unidirectional_link("host1", "host2", Link::shared(100., 0.1));
+    network.add_unidirectional_link("host2", "host3", Link::shared(100., 0.1));
+    network.add_unidirectional_link("host3", "host1", Link::shared(100., 0.1));
+
+    network.init_topology();
+    let network_rc = Rc::new(RefCell::new(network));
+    sim.add_handler("net", network_rc.clone());
+
+    let node1 = Node::new(network_rc.clone(), sim.create_context("node1"));
+    let node1_id = sim.add_handler("node1", Rc::new(RefCell::new(node1)));
+    let node2 = Node::new(network_rc.clone(), sim.create_context("node2"));
+    let node2_id = sim.add_handler("node2", Rc::new(RefCell::new(node2)));
+    let node3 = Node::new(network_rc.clone(), sim.create_context("node3"));
+    let node3_id = sim.add_handler("node3", Rc::new(RefCell::new(node3)));
+
+    network_rc.borrow_mut().set_location(node1_id, "host1");
+    network_rc.borrow_mut().set_location(node2_id, "host2");
+    network_rc.borrow_mut().set_location(node3_id, "host3");
+
+    let client = sim.create_context("client");
+
+    client.emit_now(
+        Start {
+            size: 1000.0,
+            receiver_id: node3_id,
+        },
+        node1_id,
+    );
+    client.emit_now(
+        Start {
+            size: 1000.0,
+            receiver_id: node1_id,
+        },
+        node2_id,
+    );
+    client.emit_now(
+        Start {
+            size: 1000.0,
+            receiver_id: node2_id,
+        },
+        node3_id,
+    );
+
+    sim.step_until_no_events();
+
+    assert_float_eq(sim.time(), 20.2, EPSILON);
+}
+
+#[rstest]
+fn test_diamond(#[values(RoutingImpl::Dijkstra, RoutingImpl::FloydWarshall)] routing: RoutingImpl) {
+    let mut sim = Simulation::new(123);
+
+    let network_model = Box::new(TopologyAwareNetworkModel::new());
+    let routing: Box<dyn RoutingAlgorithm> = match routing {
+        RoutingImpl::Dijkstra => Box::new(ShortestPathDijkstra::default()),
+        RoutingImpl::FloydWarshall => Box::new(ShortestPathFloydWarshall::default()),
+    };
+    let mut network = Network::with_routing(routing, network_model, sim.create_context("net"));
+
+    network.add_node("input", Box::new(ConstantBandwidthNetworkModel::new(100.0, 0.0)));
+    network.add_node("mid1", Box::new(ConstantBandwidthNetworkModel::new(100.0, 0.0)));
+    network.add_node("mid2", Box::new(ConstantBandwidthNetworkModel::new(100.0, 0.0)));
+    network.add_node("output", Box::new(ConstantBandwidthNetworkModel::new(100.0, 0.0)));
+
+    network.add_unidirectional_link("input", "mid1", Link::shared(1000., 1.));
+    network.add_unidirectional_link("mid1", "output", Link::shared(1000., 0.1));
+    network.add_unidirectional_link("input", "mid2", Link::shared(100., 0.1));
+    network.add_unidirectional_link("mid2", "output", Link::shared(100., 0.1));
+
+    network.init_topology();
+    let network_rc = Rc::new(RefCell::new(network));
+    sim.add_handler("net", network_rc.clone());
+
+    let input = Node::new(network_rc.clone(), sim.create_context("input"));
+    let input = sim.add_handler("input", Rc::new(RefCell::new(input)));
+    let output = Node::new(network_rc.clone(), sim.create_context("output"));
+    let output = sim.add_handler("output", Rc::new(RefCell::new(output)));
+
+    network_rc.borrow_mut().set_location(input, "input");
+    network_rc.borrow_mut().set_location(output, "output");
+
+    let client = sim.create_context("client");
+
+    client.emit_now(
+        Start {
+            size: 1000.0,
+            receiver_id: output,
+        },
+        input,
+    );
+
+    sim.step_until_no_events();
+
+    assert_float_eq(sim.time(), 10.2, EPSILON);
+}
