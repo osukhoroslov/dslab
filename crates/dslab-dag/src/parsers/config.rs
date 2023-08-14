acb1e312d171c0c44b1eed2feee0b44faaf113ca
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2023-08-14 20:49:55 +0300

    fix parsing of WfCommons instances, add new generation options

--- crates/dslab-dag/src/parsers/config.rs
+++ crates/dslab-dag/src/parsers/config.rs
@@ -5,8 +5,37 @@ use serde::Deserialize;
 pub struct ParserConfig {
     /// Reference machine speed in Gflop/s (default: 10).
     pub reference_speed: f64,
-    /// Whether to ignore task memory (if present) and set it to 0 (default: false).
+    /// Whether to ignore the task memory requirement (if present) and set it to 0 (default: false).
+    #[serde(default)]
     pub ignore_memory: bool,
+    /// Options for generating cores requirements for tasks.
+    pub generate_cores: Option<GenerateCoresOptions>,
+    /// Options for generating memory requirements for tasks.
+    pub generate_memory: Option<GenerateMemoryOptions>,
+    /// Random seed for cores and memory generation.
+    pub seed: Option<u64>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GenerateCoresOptions {
+    /// Minimum cores value.
+    pub min: u32,
+    /// Maximum cores value.
+    pub max: u32,
+    /// Whether to use the same value for all tasks of the same type
+    /// (inferred by the task name prefix).
+    pub regular: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GenerateMemoryOptions {
+    /// Minimum memory value.
+    pub min: u64,
+    /// Maximum memory value.
+    pub max: u64,
+    /// Whether to use the same value for all tasks of the same type
+    /// (inferred by the task name prefix).
+    pub regular: bool,
 }
 
 impl ParserConfig {
@@ -14,6 +43,9 @@ impl ParserConfig {
         Self {
             reference_speed: speed,
             ignore_memory: false,
+            generate_cores: None,
+            generate_memory: None,
+            seed: None,
         }
     }
 }
@@ -23,6 +55,9 @@ impl Default for ParserConfig {
         Self {
             reference_speed: 10.,
             ignore_memory: false,
+            generate_cores: None,
+            generate_memory: None,
+            seed: None,
         }
     }
 }
