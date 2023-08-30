3aacf5445e6836e1f99d24ffa7dff479899c5e1a
osukhoroslov <osukhoroslov@users.noreply.github.com>
2023-08-30 17:02:39 +0000

    deploy: 46de51c4e38f80671d82e30426d665e50b5d0691

--- docs/dslab_mp/mc/predicates/collects/sidebar-items.js
+++ docs/dslab_mp/mc/predicates/collects/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["event_happened_n_times_current_run","got_n_local_messages","no_events"]};
+window.SIDEBAR_ITEMS = {"fn":["any_collect","event_happened_n_times_current_run","got_n_local_messages","no_events"]};
