eed1f4c426dfd3a6a9590be8ac23d280f44423ac
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2024-03-26 00:21:00 +0300

    Improve example

--- examples/async-futures-select/src/worker.rs
+++ examples/async-futures-select/src/worker.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::{cell::RefCell, rc::Rc};
 
 use futures::future::FutureExt;
@@ -40,34 +40,36 @@ impl Worker {
 
     async fn dispatch_loop(&self) {
         let mut pending_tasks: VecDeque<TaskRequest> = VecDeque::new();
+        let mut dispatched_tasks: HashMap<u64, TaskRequest> = HashMap::new();
         loop {
             let try_dispatch = select! {
                 (_, task) = self.ctx.recv_event::<TaskRequest>().fuse() => {
                     pending_tasks.push_back(task);
                     pending_tasks.len() == 1
                 },
-                (_, comp_finished_info) = self.ctx.recv_event::<CompFinished>().fuse() => {
-                    log_debug!(self.ctx, format!("task with key {} is completed", comp_finished_info.id));
+                (_, finished_info) = self.ctx.recv_event::<CompFinished>().fuse() => {
+                    let task = dispatched_tasks.remove(&finished_info.id).unwrap();
+                    log_debug!(self.ctx, format!("task {} with key {} is completed", task.id, finished_info.id));
                     pending_tasks.len() > 0
                 }
             };
             if try_dispatch {
-                self.try_dispatch_tasks(&mut pending_tasks).await;
+                for _ in 0..pending_tasks.len() {
+                    let task = pending_tasks.front().unwrap();
+                    match self.try_dispatch_task(task).await {
+                        Some(req_id) => {
+                            dispatched_tasks.insert(req_id, pending_tasks.pop_front().unwrap());
+                        }
+                        None => {
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
 
-    async fn try_dispatch_tasks(&self, pending_tasks: &mut VecDeque<TaskRequest>) {
-        for _ in 0..pending_tasks.len() {
-            let task = pending_tasks.pop_front().unwrap();
-            if !self.try_dispatch_task(&task).await {
-                pending_tasks.push_front(task);
-                break;
-            }
-        }
-    }
-
-    async fn try_dispatch_task(&self, task: &TaskRequest) -> bool {
+    async fn try_dispatch_task(&self, task: &TaskRequest) -> Option<u64> {
         // pass task to compute and obtain request id used further as event key
         let req_id = self.compute.borrow_mut().run(
             task.flops,
@@ -81,11 +83,11 @@ impl Worker {
         select! {
             _ = self.ctx.recv_event_by_key::<CompStarted>(req_id).fuse() => {
                 log_debug!(self.ctx, format!("task {} with key {} is started", task.id, req_id));
-                true
+                Some(req_id)
             },
             (_, failed) = self.ctx.recv_event_by_key::<CompFailed>(req_id).fuse() => {
                 log_debug!(self.ctx, format!("task {} with key {} is failed: {}", task.id, req_id, json!(failed)));
-                false
+                None
             }
         }
     }
