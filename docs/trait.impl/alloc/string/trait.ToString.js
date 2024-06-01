f3ca84ad85f6aa3647b6a48ae7e8854aa2d5b64a
osukhoroslov <osukhoroslov@users.noreply.github.com>
2024-06-01 11:11:58 +0000

    deploy: b289c2d5d64ea2f075da5c083ed904467d10ae5c

--- docs/trait.impl/alloc/string/trait.ToString.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"dslab_dag":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.1/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"dslab_dag/trace_log/enum.Event.html\" title=\"enum dslab_dag::trace_log::Event\">Event</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
