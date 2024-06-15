be6f0f0053944f07769c52efe507dadb51f711b6
osukhoroslov <osukhoroslov@users.noreply.github.com>
2024-06-15 13:46:13 +0000

    deploy: 3db9055400bdd81f9a5e5586b1dfde0aecaef05c

---
+++ docs/search.desc/dslab_mp_python/dslab_mp_python-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("dslab_mp_python", 0, "Returns the argument unchanged.\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.")
