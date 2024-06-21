fab2d62f34f5c09b56dbded253e2e80d12a29c82
osukhoroslov <osukhoroslov@users.noreply.github.com>
2024-06-21 16:44:09 +0000

    deploy: 24deccac4c946f8d710e5026618aed667405aa05

--- docs/type.impl/std/primitive.u64.js
+++ docs/type.impl/std/primitive.u64.js
@@ -1,3 +1,4 @@
 (function() {var type_impls = {
-"dslab_core":[]
+"dslab_core":[],
+"dslab_models":[]
 };if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
